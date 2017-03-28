42c17f73f3e6fc82220c92d650009fa1e84160ea
meishuquan <shuquan.mei@lovelawu.com>
2017-03-28 14:15:22 +0800

    refactor:删除sms-srv

--- business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/service/MerchantService.java
+++ business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/service/MerchantService.java
@@ -1,20 +1,14 @@
 package com.lawu.eshop.merchant.api.service;
 
 
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.lawu.eshop.framework.core.page.Page;
+import com.lawu.eshop.framework.core.page.PageParam;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.user.dto.LoginUserDTO;
-import com.lawu.eshop.user.dto.MerchantInviterDTO;
+import com.lawu.eshop.user.dto.MerchantDTO;
 import com.lawu.eshop.user.param.RegisterParam;
-import com.lawu.eshop.user.query.MerchantInviterParam;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author meishuquan
@@ -59,7 +53,7 @@ public interface MerchantService {
      */
     @RequestMapping(method = RequestMethod.POST, value = "merchant/register")
     Result register(@ModelAttribute RegisterParam registerParam);
-    
+
 
 
     /**
@@ -80,6 +74,5 @@ public interface MerchantService {
      */
     @RequestMapping(method = RequestMethod.GET, value = "merchant/getMerchant/{account}")
     Result getMerchantByAccount(@PathVariable("account") String account);
-
 
 }
