e80e586612d77d86e0aba428c7998aca53de931b
zhangyong <yong.zhang@lovelawu.com>
2017-03-28 14:49:13 +0800

    refactor：增加图片枚举类

---
+++ dto/user-dto/src/main/java/com/lawu/eshop/user/dto/MerchantStoreImageEnum.java
@@ -0,0 +1,18 @@
+package com.lawu.eshop.user.dto;
+
+/**
+ *
+ * Created by zhangyong on 2017/3/28.
+ */
+public enum MerchantStoreImageEnum {
+    STORE_IMAGE_STORE((byte)0x01),
+    STORE_IMAGE_ENVIRONMENT((byte)0x02),
+    STORE_IMAGE_LOGO((byte)0x03),
+    STORE_IMAGE_LICENSE((byte)0x04),
+    STORE_IMAGE_OTHER((byte)0x05),
+    STORE_IMAGE_IDCARD((byte)0x06);
+    public Byte val;
+    MerchantStoreImageEnum(Byte val) {
+        this.val = val;
+    }
+}
