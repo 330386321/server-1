b302e422e8c0285683a8d8be96de0cafffeb4ad5
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-03-28 16:03:27 +0800

    冲突解决

---
+++ dto/user-dto/src/main/java/com/lawu/eshop/user/dto/MemberProfileDTO.java
@@ -0,0 +1,27 @@
+package com.lawu.eshop.user.dto;
+
+public class MemberProfileDTO {
+	
+	private Integer inviteMemberCount;
+	
+	private Integer inviteMerchantCount;
+
+	public Integer getInviteMemberCount() {
+		return inviteMemberCount;
+	}
+
+	public void setInviteMemberCount(Integer inviteMemberCount) {
+		this.inviteMemberCount = inviteMemberCount;
+	}
+
+	public Integer getInviteMerchantCount() {
+		return inviteMerchantCount;
+	}
+
+	public void setInviteMerchantCount(Integer inviteMerchantCount) {
+		this.inviteMerchantCount = inviteMerchantCount;
+	}
+	
+	
+
+}
