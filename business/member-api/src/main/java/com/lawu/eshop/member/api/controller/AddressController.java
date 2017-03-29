33b53b974e2993cd0bf37cd2ae24875a56f06cbd
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-03-29 18:23:37 +0800

    refactor:(member-api)地址 token 授权处理

--- business/member-api/src/main/java/com/lawu/eshop/member/api/controller/AddressController.java
+++ business/member-api/src/main/java/com/lawu/eshop/member/api/controller/AddressController.java
@@ -47,7 +47,7 @@ public class AddressController extends BaseController {
 	@ApiOperation(value = "收货地址信息查询", notes = "根据会员id获取收货地址列表信息[1000|1001]（张荣成）", httpMethod = "GET")
 	@ApiResponse(code = HttpCode.SC_OK, message = "success")
 	@RequestMapping(value = "selectAddress", method = RequestMethod.GET)
-    public Result<List<AddressDTO>> selectAddress() {
+    public Result<List<AddressDTO>> selectAddress(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token) {
 		Long userId=UserUtil.getCurrentUserId(getRequest());
 		Result<List<AddressDTO>> addressDTOS = addressService.selectAddress(userId);
         return addressDTOS;
@@ -61,7 +61,7 @@ public class AddressController extends BaseController {
 	@ApiOperation(value = "查询单个收货地址", notes = "单个查询收货地址[1000|1001]（张荣成）", httpMethod = "GET")
 	@ApiResponse(code = HttpCode.SC_OK, message = "success")
 	@RequestMapping(value = "get/{id}", method = RequestMethod.GET)
-    public Result<AddressDTO> get(@RequestParam  @ApiParam (required = true, value = "收货地址id") Long id) {
+    public Result<AddressDTO> get(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token,@RequestParam  @ApiParam (required = true, value = "收货地址id") Long id) {
 		Result<AddressDTO> addressDTO=addressService.get(id);
 		return addressDTO;
     }
@@ -74,7 +74,7 @@ public class AddressController extends BaseController {
 	@ApiOperation(value = "删除收货地址", notes = "删除收货地址[1000|1001|1002]（张荣成）", httpMethod = "DELETE")
 	@ApiResponse(code = HttpCode.SC_NO_CONTENT, message = "success")
 	@RequestMapping(value = "remove/{id}", method = RequestMethod.DELETE)
-    public Result remove(@RequestParam @ApiParam (required = true, value = "收货地址id") Long id) {
+    public Result remove(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token,@RequestParam @ApiParam (required = true, value = "收货地址id") Long id) {
 		Result rs=addressService.delete(id);
 		return rs;
     }
@@ -101,7 +101,7 @@ public class AddressController extends BaseController {
 	@ApiOperation(value = "修改收货地址", notes = "修改收货地址[1000|1001]（张荣成）", httpMethod = "POST")
 	@ApiResponse(code = HttpCode.SC_CREATED, message = "success")
 	@RequestMapping(value = "update/{id}", method = RequestMethod.POST)
-    public Result update(@RequestParam @ApiParam (required = true, value = "收货地址id") Long id,
+    public Result update(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token,@RequestParam @ApiParam (required = true, value = "收货地址id") Long id,
     		@ModelAttribute  @ApiParam(required = true, value = "收货地址信息") AddressParam address) {
 		Result rs= addressService.update(address,id);
 		return rs;
