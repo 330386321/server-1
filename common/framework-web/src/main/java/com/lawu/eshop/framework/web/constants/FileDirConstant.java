50657343efd77e77f8bcbc0ba0e5dc42f57f0b0d
zhangyong <yong.zhang@lovelawu.com>
2017-03-29 10:59:51 +0800

    refactor：新增头像存储dir

--- common/framework-web/src/main/java/com/lawu/eshop/framework/web/constants/FileDirConstant.java
+++ common/framework-web/src/main/java/com/lawu/eshop/framework/web/constants/FileDirConstant.java
@@ -2,9 +2,12 @@ package com.lawu.eshop.framework.web.constants;
 
 public class FileDirConstant {
 
-	//门店图片路径
-	public static final String DIR_STORE = "store";
-	
-	//商品图片路径
-	public static final String DIR_PRODUCT = "product";
+    //门店图片路径
+    public static final String DIR_STORE = "store";
+
+    //商品图片路径
+    public static final String DIR_PRODUCT = "product";
+
+    //会员头像路径
+    public static final String DIR_HEAD = "head";
 }
