63356620f640ab0c467b128cae576ce143f80d79
sunlinqing <linqing.sun@lovelawu.com>
2017-03-29 16:41:55 +0800

    doc: 只对注解了@ApiOperation的方法生成文档

--- business/member-api/src/main/java/com/lawu/eshop/member/api/doc/SwaggerConfig.java
+++ business/member-api/src/main/java/com/lawu/eshop/member/api/doc/SwaggerConfig.java
@@ -2,11 +2,13 @@ package com.lawu.eshop.member.api.doc;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -29,6 +31,7 @@ public class SwaggerConfig {
                 .forCodeGeneration(true)
                 .pathMapping("/")
                 .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(paths())
                 .build()
                 .apiInfo(apiInfo())
