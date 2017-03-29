5cce85cf2c57bef5a5037445d138bc9fbdbc5c9f
zhangyong <yong.zhang@lovelawu.com>
2017-03-29 10:58:49 +0800

    refactor：增加门店图片回显判断

--- business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/controller/MerchantStoreController.java
+++ business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/controller/MerchantStoreController.java
@@ -4,6 +4,7 @@ import com.lawu.eshop.framework.web.BaseController;
 import com.lawu.eshop.framework.web.HttpCode;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.framework.web.ResultCode;
+import com.lawu.eshop.framework.web.constants.FileDirConstant;
 import com.lawu.eshop.merchant.api.service.MerchantStoreService;
 import com.lawu.eshop.user.dto.CertifTypeEnum;
 import com.lawu.eshop.user.dto.MerchantStoreDTO;
@@ -15,8 +16,13 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import util.UploadFileUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -48,41 +54,83 @@ public class MerchantStoreController extends BaseController {
     public Result saveMerchantStoreInfo(@PathVariable("merchantId") Long merchantId, @ModelAttribute @ApiParam MerchantStoreParam merchantStoreParam, @RequestParam("storeType") MerchantStoreTypeEnum storeType,
                                         @RequestParam("certifType") CertifTypeEnum certifType) {
         HttpServletRequest request = getRequest();
-        Map<String, String> map = null;//UploadFileService.uploadStoreImages(request);
-        //上传返回成功/失败
-        String flag = map.get("resultFlag");
-        if ("OK".equals(flag)) {
-            String storeUrl = map.get("storeUrl");
-            String environmentUrl = map.get("environmentUrl");
-            String logoUrl = map.get("logoUrl");
-            String idcardUrl = map.get("idcardUrl");
-            String licenceUrl = map.get("licenceUrl");
-            String otherUrl = map.get("otherUrl");
-            if (!"".equals(storeUrl)) {
-                merchantStoreParam.setStoreUrl(storeUrl);
+        StringBuffer idCardUrls = new StringBuffer();        //身份证照
+        StringBuffer storeUrls = new StringBuffer();        //门店照
+        StringBuffer environmentUrls = new StringBuffer();        //店内环境照
+        StringBuffer storeLogoUrls = new StringBuffer();    //店铺logo
+        StringBuffer licenseUrls = new StringBuffer();        //营业执照
+        StringBuffer otherUrls = new StringBuffer();
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (multipartResolver.isMultipart(request)) {
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+            Iterator<String> iter = multiRequest.getFileNames();
+            while (iter.hasNext()) {
+                MultipartFile file = multiRequest.getFile(iter.next());
+                String fileName = file.getName();
+                Map<String, String> map = UploadFileUtil.uploadOnePic(request, file, FileDirConstant.DIR_STORE);
+                String flag = map.get("resultFlag");
+                if ("0".equals(flag)) {
+                    //有图片上传成功返回,拼接图片url
+                    String imgUrl = map.get("imgUrl");
+                    if (fileName.contains("store") && !"".equals(imgUrl)) {
+                        storeUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("environment") && !"".equals(imgUrl)) {
+                        environmentUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("logo") && !"".equals(imgUrl)) {
+                        storeLogoUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("idcard") && !"".equals(imgUrl)) {
+                        idCardUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("licence") && !"".equals(imgUrl)) {
+                        licenseUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("other") && !"".equals(imgUrl)) {
+                        otherUrls.append(imgUrl + ",");
+                    }
+                } else {
+                    return failCreated(Integer.valueOf(flag));
+                }
             }
-            if (!"".equals(environmentUrl)) {
-                merchantStoreParam.setEnvironmentUrl(environmentUrl);
+            //判断回显照片
+            if (!"".equals(merchantStoreParam.getStoreUrl())) {
+                merchantStoreParam.setStoreUrl(otherUrls + merchantStoreParam.getStoreUrl());
+            } else {
+                merchantStoreParam.setStoreUrl(otherUrls.toString());
             }
-            if (!"".equals(logoUrl)) {
-                merchantStoreParam.setLogoUrl(logoUrl);
+            if (!"".equals(merchantStoreParam.getEnvironmentUrl())) {
+                merchantStoreParam.setEnvironmentUrl(environmentUrls + merchantStoreParam.getEnvironmentUrl());
+            } else {
+                merchantStoreParam.setEnvironmentUrl(environmentUrls.toString());
             }
-            if (!"".equals(idcardUrl)) {
-                merchantStoreParam.setIdcardUrl(idcardUrl);
+            if (!"".equals(merchantStoreParam.getLogoUrl())) {
+                merchantStoreParam.setLogoUrl(storeLogoUrls + merchantStoreParam.getLogoUrl());
+            } else {
+                merchantStoreParam.setLogoUrl(storeLogoUrls.toString());
             }
-            if (!"".equals(licenceUrl)) {
-                merchantStoreParam.setLicenseUrl(licenceUrl);
+            if (!"".equals(merchantStoreParam.getIdcardUrl())) {
+                merchantStoreParam.setIdcardUrl(idCardUrls + merchantStoreParam.getIdcardUrl());
+            } else {
+                merchantStoreParam.setIdcardUrl(idCardUrls.toString());
             }
-            if (!"".equals(otherUrl)) {
-                merchantStoreParam.setOtherUrl(otherUrl);
+            if (!"".equals(merchantStoreParam.getLicenseUrl())) {
+                merchantStoreParam.setLicenseUrl(licenseUrls + merchantStoreParam.getLicenseUrl());
+            } else {
+                merchantStoreParam.setLicenseUrl(licenseUrls.toString());
             }
-            return merchantStoreService.saveMerchantStoreInfo(merchantId, merchantStoreParam, storeType, certifType);
+            if (!"".equals(merchantStoreParam.getOtherUrl())) {
+                merchantStoreParam.setOtherUrl(otherUrls + merchantStoreParam.getOtherUrl());
+            } else {
+                merchantStoreParam.setOtherUrl(otherUrls.toString());
+            }
 
+            merchantStoreParam.setStoreUrl(storeUrls.toString());
+            return merchantStoreService.saveMerchantStoreInfo(merchantId, merchantStoreParam, storeType, certifType);
         }
+
         return failCreated(ResultCode.FAIL);
-        //Map<String,String> map = UploadFileService.uploadStoreImages(request);
-        //if()
-        //return merchantStoreService.saveMerchantStoreInfo(merchantId, merchantStoreParam, storeType, certifType);
     }
 
     @ApiOperation(value = "修改门店信息", notes = "错误信息 [1012]（章勇）", httpMethod = "PUT")
@@ -92,56 +140,83 @@ public class MerchantStoreController extends BaseController {
     public Result updateMerchantStoreInfo(@PathVariable("merchantId") Long merchantId, @ModelAttribute @ApiParam MerchantStoreParam merchantStoreParam, @RequestParam("storeType") MerchantStoreTypeEnum storeType,
                                           @RequestParam("certifType") CertifTypeEnum certifType) {
         HttpServletRequest request = getRequest();
-        Map<String, String> urlMap = null;//UploadFileService.uploadStoreImages(request);
-        //上传返回成功/失败
-        String urlFlag = urlMap.get("resultFlag");
-        if ("OK".equals(urlFlag)) {
-            String storeUrl = urlMap.get("storeUrl");
-            String environmentUrl = urlMap.get("environmentUrl");
-            String logoUrl = urlMap.get("logoUrl");
-            String idcardUrl = urlMap.get("idcardUrl");
-            String licenceUrl = urlMap.get("licenceUrl");
-            String otherUrl = urlMap.get("otherUrl");
-            if (!"".equals(storeUrl) && "".equals(merchantStoreParam.getStoreUrl())) {
-                merchantStoreParam.setStoreUrl(storeUrl);
-            }
-            if (!"".equals(environmentUrl) && "".equals(merchantStoreParam.getEnvironmentUrl())) {
-                merchantStoreParam.setEnvironmentUrl(environmentUrl);
-            }
-            if (!"".equals(logoUrl) && "".equals(merchantStoreParam.getLogoUrl())) {
-                merchantStoreParam.setLogoUrl(logoUrl);
-            }
-            if (!"".equals(idcardUrl) && "".equals(merchantStoreParam.getIdcardUrl())) {
-                merchantStoreParam.setIdcardUrl(idcardUrl);
-            }
-            if (!"".equals(licenceUrl) && "".equals(merchantStoreParam.getLicenseUrl())) {
-                merchantStoreParam.setLicenseUrl(licenceUrl);
-            }
-            if (!"".equals(otherUrl) && "".equals(merchantStoreParam.getOtherUrl())) {
-                merchantStoreParam.setOtherUrl(otherUrl);
+        StringBuffer idCardUrls = new StringBuffer();        //身份证照
+        StringBuffer storeUrls = new StringBuffer();        //门店照
+        StringBuffer environmentUrls = new StringBuffer();        //店内环境照
+        StringBuffer storeLogoUrls = new StringBuffer();    //店铺logo
+        StringBuffer licenseUrls = new StringBuffer();        //营业执照
+        StringBuffer otherUrls = new StringBuffer();
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (multipartResolver.isMultipart(request)) {
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+            Iterator<String> iter = multiRequest.getFileNames();
+            while (iter.hasNext()) {
+                MultipartFile file = multiRequest.getFile(iter.next());
+                String fileName = file.getName();
+                Map<String, String> map = UploadFileUtil.uploadOnePic(request, file, FileDirConstant.DIR_STORE);
+                String flag = map.get("resultFlag");
+                if ("0".equals(flag)) {
+                    //有图片上传成功返回,拼接图片url
+                    String imgUrl = map.get("imgUrl");
+                    if (fileName.contains("store") && !"".equals(imgUrl)) {
+                        storeUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("environment") && !"".equals(imgUrl)) {
+                        environmentUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("logo") && !"".equals(imgUrl)) {
+                        storeLogoUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("idcard") && !"".equals(imgUrl)) {
+                        idCardUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("licence") && !"".equals(imgUrl)) {
+                        licenseUrls.append(imgUrl + ",");
+                    }
+                    if (fileName.contains("other") && !"".equals(imgUrl)) {
+                        otherUrls.append(imgUrl + ",");
+                    }
+
+                } else {
+                    return failCreated(Integer.valueOf(flag));
+                }
             }
 
-
+            //判断回显照片
+            if (!"".equals(merchantStoreParam.getStoreUrl())) {
+                merchantStoreParam.setStoreUrl(otherUrls + merchantStoreParam.getStoreUrl());
+            } else {
+                merchantStoreParam.setStoreUrl(otherUrls.toString());
+            }
+            if (!"".equals(merchantStoreParam.getEnvironmentUrl())) {
+                merchantStoreParam.setEnvironmentUrl(environmentUrls + merchantStoreParam.getEnvironmentUrl());
+            } else {
+                merchantStoreParam.setEnvironmentUrl(environmentUrls.toString());
+            }
+            if (!"".equals(merchantStoreParam.getLogoUrl())) {
+                merchantStoreParam.setLogoUrl(storeLogoUrls + merchantStoreParam.getLogoUrl());
+            } else {
+                merchantStoreParam.setLogoUrl(storeLogoUrls.toString());
+            }
+            if (!"".equals(merchantStoreParam.getIdcardUrl())) {
+                merchantStoreParam.setIdcardUrl(idCardUrls + merchantStoreParam.getIdcardUrl());
+            } else {
+                merchantStoreParam.setIdcardUrl(idCardUrls.toString());
+            }
+            if (!"".equals(merchantStoreParam.getLicenseUrl())) {
+                merchantStoreParam.setLicenseUrl(licenseUrls + merchantStoreParam.getLicenseUrl());
+            } else {
+                merchantStoreParam.setLicenseUrl(licenseUrls.toString());
+            }
+            if (!"".equals(merchantStoreParam.getOtherUrl())) {
+                merchantStoreParam.setOtherUrl(otherUrls + merchantStoreParam.getOtherUrl());
+            } else {
+                merchantStoreParam.setOtherUrl(otherUrls.toString());
+            }
             return merchantStoreService.updateMerchantStoreInfo(merchantId, merchantStoreParam, storeType, certifType);
-
-        } else
-            return failCreated(ResultCode.FAIL);
-    }
-
-   /* @ApiOperation(value = "测试上传", notes = "错误信息 [1012]（章勇）", httpMethod = "POST")
-    //@Authorization
-    @ApiResponse(code = HttpCode.SC_CREATED, message = "success")
-    @RequestMapping(value = "testUpload", method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
-    public Result testUpload(@RequestParam("file") MultipartFile file) {
-        HttpServletRequest request = getRequest();
-        Map<String,String> map =  UploadFileService.uploadOnePic(request,file);
-        String flag = map.get("resultFlag");
-        if("OK".equals(flag)){
-            String url = map.get("imgUrl");
         }
-        Result r = new Result();
-        return r;
 
-    }*/
+        return failCreated(ResultCode.FAIL);
+    }
 
 }
