aeede811834fc0af5b3d14ff2e0fceeab1c1b372
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-03-29 14:21:21 +0800

    fix(property-srv):数据库表字段修改userNo>>>userNum

--- service-share/property-srv/src/main/java/com/lawu/eshop/property/srv/service/impl/PropertyInfoServiceImpl.java
+++ service-share/property-srv/src/main/java/com/lawu/eshop/property/srv/service/impl/PropertyInfoServiceImpl.java
@@ -1,17 +1,20 @@
 package com.lawu.eshop.property.srv.service.impl;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lawu.eshop.property.srv.bo.PropertyBalanceBO;
 import com.lawu.eshop.property.srv.bo.PropertyInfoBO;
+import com.lawu.eshop.property.srv.converter.PropertyBalanceConverter;
 import com.lawu.eshop.property.srv.converter.PropertyInfoConverter;
 import com.lawu.eshop.property.srv.domain.PropertyInfoDO;
 import com.lawu.eshop.property.srv.domain.PropertyInfoDOExample;
 import com.lawu.eshop.property.srv.mapper.PropertyInfoDOMapper;
 import com.lawu.eshop.property.srv.service.PropertyInfoService;
 import com.lawu.eshop.utils.MD5;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 /**
  * 资产管理服务实现
@@ -22,32 +25,45 @@ import java.util.List;
 @Service
 public class PropertyInfoServiceImpl implements PropertyInfoService {
 
-    @Autowired
-    private PropertyInfoDOMapper propertyInfoDOMapper;
+	@Autowired
+	private PropertyInfoDOMapper propertyInfoDOMapper;
 
-    @Override
-    public void savePropertyInfo(String userNo) {
-        PropertyInfoDO propertyInfoDO = new PropertyInfoDO();
-        propertyInfoDO.setUserNo(userNo);
-        propertyInfoDO.setGmtCreate(new Date());
-        propertyInfoDOMapper.insertSelective(propertyInfoDO);
-    }
+	@Override
+	public void savePropertyInfo(String userNum) {
+		PropertyInfoDO propertyInfoDO = new PropertyInfoDO();
+		propertyInfoDO.setUserNum(userNum);
+		propertyInfoDO.setGmtCreate(new Date());
+		propertyInfoDOMapper.insertSelective(propertyInfoDO);
+	}
 
-    @Override
-    public PropertyInfoBO getPropertyInfoByUserNo(String userNo) {
-        PropertyInfoDOExample propertyInfoDOExample = new PropertyInfoDOExample();
-        propertyInfoDOExample.createCriteria().andUserNoEqualTo(userNo);
-        List<PropertyInfoDO> propertyInfoDOS = propertyInfoDOMapper.selectByExample(propertyInfoDOExample);
-        return propertyInfoDOS.isEmpty() ? null : PropertyInfoConverter.convertBO(propertyInfoDOS.get(0));
-    }
+	@Override
+	public PropertyInfoBO getPropertyInfoByUserNo(String userNo) {
+		PropertyInfoDOExample propertyInfoDOExample = new PropertyInfoDOExample();
+		propertyInfoDOExample.createCriteria().andUserNumEqualTo(userNo);
+		List<PropertyInfoDO> propertyInfoDOS = propertyInfoDOMapper.selectByExample(propertyInfoDOExample);
+		return propertyInfoDOS.isEmpty() ? null : PropertyInfoConverter.convertBO(propertyInfoDOS.get(0));
+	}
 
-    @Override
-    public void updatePayPwd(String userNo, String originalPwd, String newPwd) {
-        PropertyInfoDO propertyInfoDO = new PropertyInfoDO();
-        propertyInfoDO.setUserNo(userNo);
-        propertyInfoDO.setPayPassword(MD5.MD5Encode(newPwd));
-        PropertyInfoDOExample propertyInfoDOExample = new PropertyInfoDOExample();
-        propertyInfoDOExample.createCriteria().andUserNoEqualTo(userNo).andPayPasswordEqualTo(MD5.MD5Encode(newPwd));
-        propertyInfoDOMapper.updateByExampleSelective(propertyInfoDO, propertyInfoDOExample);
-    }
+	@Override
+	public void updatePayPwd(String userNum, String originalPwd, String newPwd) {
+		PropertyInfoDO propertyInfoDO = new PropertyInfoDO();
+		propertyInfoDO.setUserNum(userNum);
+		propertyInfoDO.setPayPassword(MD5.MD5Encode(newPwd));
+		PropertyInfoDOExample propertyInfoDOExample = new PropertyInfoDOExample();
+		propertyInfoDOExample.createCriteria().andUserNumEqualTo(userNum).andPayPasswordEqualTo(MD5.MD5Encode(newPwd));
+		propertyInfoDOMapper.updateByExampleSelective(propertyInfoDO, propertyInfoDOExample);
+	}
+
+	@Override
+	public PropertyBalanceBO getPropertyBalanceByUserNum(String userNum) {
+		PropertyInfoDOExample propertyInfoDOExample = new PropertyInfoDOExample();
+		propertyInfoDOExample.createCriteria().andUserNumEqualTo(userNum);
+		List<PropertyInfoDO> propertyInfoDOS = propertyInfoDOMapper.selectByExample(propertyInfoDOExample);
+
+		if (propertyInfoDOS == null || propertyInfoDOS.isEmpty()) {
+			return null;
+		}
+
+		return PropertyBalanceConverter.convert(propertyInfoDOS.get(0));
+	}
 }
