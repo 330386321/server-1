0e36c0accf73b38948d77362c23ddbe67a756818
sunlinqing <linqing.sun@lovelawu.com>
2017-04-01 17:01:52 +0800

    refactor(framework-core): 分页参数优化

---
+++ common/framework-core/src/main/java/com/lawu/eshop/framework/core/page/AbstractPageParam.java
@@ -0,0 +1,45 @@
+package com.lawu.eshop.framework.core.page;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 分页参数
+ * @author Leach
+ * @date 2017/4/1
+ */
+public abstract class AbstractPageParam {
+
+    /**
+     * 当前页码
+     */
+    @ApiModelProperty(value = "当前页码", required = true)
+    private Integer currentPage = 0;
+
+    /**
+     * 每页数量
+     */
+    @ApiModelProperty(value = "每页数量", required = true)
+    private Integer pageSize = 20;
+
+
+    public int getOffset() {
+        return this.pageSize * this.currentPage;
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+}
