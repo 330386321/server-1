59f4c658fe5d61f0aacb7b71841e03d113988573
zhangyong <yong.zhang@lovelawu.com>
2017-04-06 10:07:01 +0800

    refactor：新增上传图片Util

--- common/media/src/main/java/util/UploadFileUtil.java
+++ common/media/src/main/java/util/UploadFileUtil.java
@@ -1,90 +1,228 @@
 package util;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.lawu.eshop.utils.RandomUtil;
 import com.lawu.eshop.utils.ValidateUtil;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 /**
  * 文件上传工具类
  * Created by zhangyong on 2017/3/29.
  */
 public class UploadFileUtil {
-	/**
-	 * @param request
-	 * @param file
-	 *            单个文件
-	 * @param dir
-	 *            业务的图片文件夹，值参考com.lawu.eshop.framework.web.constants.
-	 *            FileDirConstant
-	 * @return resultFlag参考ResultCode
-	 */
-	public static Map<String, String> uploadOnePic(HttpServletRequest request, MultipartFile file, String dir) {
-		Map<String, String> valsMap = new HashMap<>();
-		// 设置默认返回类型成功
-		valsMap.put("resultFlag", "0");
-		String basePath = request.getServletContext().getRealPath("/");// 根路径
-		String originalFilename = file.getOriginalFilename();
-		if (originalFilename != null && !"".equals(originalFilename)) {
-			String prefix = originalFilename.substring(originalFilename.lastIndexOf("."));
-			prefix = prefix.toLowerCase();
-			String newfileName = RandomUtil.buildFileName(prefix);
-			if (!ValidateUtil.validateImageFormat(prefix)) {
-				valsMap.put("resultFlag", "1011");
-				valsMap.put("msg", "上传图片格式错误");
-				return valsMap;
-			}
-			// 1M=1024k=1048576字节
-			long fileSize = file.getSize();
-			if (fileSize > 0.5 * 1048576) {
-				valsMap.put("resultFlag", "1015");
-				valsMap.put("msg", "图片文件大于500K");
-				return valsMap;
-			}
-			try {
-				String imagePath = getUploadImagePath(dir).toString().replace("\\", "/");
-				String newFileUploadPath = request.getSession().getServletContext().getRealPath(imagePath);
-				File localFile = new File(newFileUploadPath + newfileName);
-				if (!localFile.getParentFile().exists()) {
-					localFile.getParentFile().mkdirs();
-				}
-				file.transferTo(localFile);
-				valsMap.put("imgUrl", basePath + imagePath + newfileName);
-				valsMap.put("resultFlag", "0");
+    /**
+     * @param request
+     * @param file    单个文件
+     * @param dir     业务的图片文件夹，值参考com.lawu.eshop.framework.web.constants.
+     *                FileDirConstant
+     * @return resultFlag参考ResultCode
+     */
+    public static Map<String, String> uploadOnePic(HttpServletRequest request, MultipartFile file, String dir) {
+        Map<String, String> valsMap = new HashMap<>();
+        // 设置默认返回类型成功
+        valsMap.put("resultFlag", "0");
+        String basePath = request.getServletContext().getRealPath("/");// 根路径
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename != null && !"".equals(originalFilename)) {
+            String prefix = originalFilename.substring(originalFilename.lastIndexOf("."));
+            prefix = prefix.toLowerCase();
+            String newfileName = RandomUtil.buildFileName(prefix);
+            if (!ValidateUtil.validateImageFormat(prefix)) {
+                valsMap.put("resultFlag", "1011");
+                valsMap.put("msg", "上传图片格式错误");
+                return valsMap;
+            }
+            // 1M=1024k=1048576字节
+            long fileSize = file.getSize();
+            if (fileSize > 0.5 * 1048576) {
+                valsMap.put("resultFlag", "1015");
+                valsMap.put("msg", "图片文件大于500K");
+                return valsMap;
+            }
+            try {
+                String imagePath = getUploadImagePath(dir).toString().replace("\\", "/");
+                String newFileUploadPath = request.getSession().getServletContext().getRealPath(imagePath);
+                File localFile = new File(newFileUploadPath + newfileName);
+                if (!localFile.getParentFile().exists()) {
+                    localFile.getParentFile().mkdirs();
+                }
+                file.transferTo(localFile);
+                valsMap.put("imgUrl", basePath + imagePath + newfileName);
+                valsMap.put("resultFlag", "0");
 
-			} catch (Exception e) {
-				e.printStackTrace();
-				valsMap.put("resultFlag", "1010");
-				valsMap.put("msg", "上传图片失败");
-			}
-		}
-		return valsMap;
-	}
+            } catch (Exception e) {
+                e.printStackTrace();
+                valsMap.put("resultFlag", "1010");
+                valsMap.put("msg", "上传图片失败");
+            }
+        }
+        return valsMap;
+    }
 
-	/**
-	 * 获取image上传路径
-	 *
-	 * @return
-	 */
-	public static StringBuffer getUploadImagePath(String dir) {
-		Calendar toDay = Calendar.getInstance();
-		StringBuffer fileUploadPath = new StringBuffer();
-		int intMonth = toDay.get(Calendar.MONTH) + 1;
-		String strMonth = "";
-		if (intMonth < 10) {
-			strMonth = "0" + String.valueOf(intMonth);
-		} else {
-			strMonth = String.valueOf(intMonth);
-		}
-		fileUploadPath.append(File.separator).append(dir).append(File.separator).append("image").append(File.separator)
-				.append(toDay.get(Calendar.YEAR)).append(File.separator).append(strMonth)
-				.append(toDay.get(Calendar.DATE)).append(File.separator);
+    /**
+     * 获取image上传路径
+     *
+     * @return
+     */
+    public static StringBuffer getUploadImagePath(String dir) {
+        Calendar toDay = Calendar.getInstance();
+        StringBuffer fileUploadPath = new StringBuffer();
+        int intMonth = toDay.get(Calendar.MONTH) + 1;
+        String strMonth = "";
+        if (intMonth < 10) {
+            strMonth = "0" + String.valueOf(intMonth);
+        } else {
+            strMonth = String.valueOf(intMonth);
+        }
+        fileUploadPath.append(File.separator).append(dir).append(File.separator).append("image").append(File.separator)
+                .append(toDay.get(Calendar.YEAR)).append(File.separator).append(strMonth)
+                .append(toDay.get(Calendar.DATE)).append(File.separator);
 
-		return fileUploadPath;
-	}
+        return fileUploadPath;
+    }
+
+    /**
+     * 文件上传单张图片
+     *
+     * @param request
+     * @param dir     文件存放目录
+     * @return
+     */
+    public static Map<String, String> uploadOneImage(HttpServletRequest request, String dir) {
+        Map<String, String> valsMap = new HashMap<>();
+        // 设置默认返回类型成功
+        valsMap.put("resultFlag", "0");
+        String bashdir = request.getServletContext().getRealPath("/");// 根路径
+        FileOutputStream out = null;
+        byte b[] = new byte[1024 * 1024];
+        Collection<Part> parts;
+
+        String urlImg = "";
+        try {
+            parts = request.getParts();
+            for (Part part : parts) {
+                if (part.getContentType() == null) {
+                    valsMap.put(part.getName(), request.getParameter(part.getName()));
+                } else {
+
+                    String extension = part.getSubmittedFileName();
+                    extension = extension.substring(extension.lastIndexOf("."));
+                    String newfileName = RandomUtil.buildFileName(extension);
+                    if (!ValidateUtil.validateImageFormat(extension)) {
+                        valsMap.put("resultFlag", "1011");
+                        valsMap.put("msg", "上传图片格式错误");
+                        return valsMap;
+                    }
+                    // 1M=1024k=1048576字节
+                    long fileSize = part.getSize();
+                    if (fileSize > 0.5 * 1048576) {
+                        valsMap.put("resultFlag", "1015");
+                        valsMap.put("msg", "图片文件大于500K");
+                        return valsMap;
+                    }
+                    File file = new File(bashdir, dir);
+                    if (!file.exists()) {
+                        file.mkdirs();
+                    }
+
+                    InputStream in = part.getInputStream();
+                    out = new FileOutputStream(new File(file, newfileName));
+
+                    int index = 0;
+                    while ((index = in.read(b)) != -1) {
+                        out.write(b, 0, index);
+                    }
+                    out.flush();
+                    out.close();
+                    in.close();
+
+                    //文件路径，文件类型
+                    urlImg = bashdir + dir + "/" + newfileName;
+
+                }
+            }
+            valsMap.put("imgUrl", urlImg);
+            valsMap.put("resultFlag", "0");
+        } catch (Exception e) {
+            valsMap.put("resultFlag", "1010");
+            valsMap.put("msg", "上传图片失败");
+            e.printStackTrace();
+        }
+        return valsMap;
+
+    }
+
+
+    /**
+     * 上传多张图片
+     * @param request
+     * @param dir
+     * @param part
+     * @return
+     */
+    public static Map<String, String> uploadImages(HttpServletRequest request, String dir, Part part) {
+        Map<String, String> valsMap = new HashMap<>();
+        // 设置默认返回类型成功
+        valsMap.put("resultFlag", "0");
+        String bashdir = request.getServletContext().getRealPath("/");// 根路径
+        FileOutputStream out = null;
+        byte b[] = new byte[1024 * 1024];
+        String urlImg = "";
+        if (part.getContentType() == null) {
+            valsMap.put(part.getName(), request.getParameter(part.getName()));
+        } else {
+
+            String extension = part.getSubmittedFileName();
+            extension = extension.substring(extension.lastIndexOf("."));
+            String newfileName = RandomUtil.buildFileName(extension);
+            if (!ValidateUtil.validateImageFormat(extension)) {
+                valsMap.put("resultFlag", "1011");
+                valsMap.put("msg", "上传图片格式错误");
+                return valsMap;
+            }
+            // 1M=1024k=1048576字节
+            long fileSize = part.getSize();
+            if (fileSize > 0.5 * 1048576) {
+                valsMap.put("resultFlag", "1015");
+                valsMap.put("msg", "图片文件大于500K");
+                return valsMap;
+            }
+            File file = new File(bashdir, dir);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+
+            InputStream in = null;
+            try {
+                in = part.getInputStream();
+                out = new FileOutputStream(new File(file, newfileName));
+                int index = 0;
+                while ((index = in.read(b)) != -1) {
+                    out.write(b, 0, index);
+                }
+
+                out.flush();
+                out.close();
+                in.close();
+                //文件路径，文件类型
+                urlImg = bashdir + dir + "/" + newfileName;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        valsMap.put("imgUrl", urlImg);
+        valsMap.put("resultFlag", "0");
+
+        return valsMap;
+
+    }
+
+
 }
