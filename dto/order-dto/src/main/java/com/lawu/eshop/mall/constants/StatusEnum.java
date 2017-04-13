11baf55e9de12dd8025c1ad464cb9875428587c4
zhangyong <yong.zhang@lovelawu.com>
2017-04-13 15:38:05 +0800

    refactor：买单记录状态增加待支付、成功、失败

--- dto/order-dto/src/main/java/com/lawu/eshop/mall/constants/StatusEnum.java
+++ dto/order-dto/src/main/java/com/lawu/eshop/mall/constants/StatusEnum.java
@@ -5,8 +5,10 @@ package com.lawu.eshop.mall.constants;
  * @date 2017/4/11.
  */
 public enum StatusEnum {
-    STATUS_VALID((byte) 0x01),//有效
-    STATUS_INVALID((byte) 0x00);//无效
+    STATUS_DEL((byte) 0x00),//0：删除
+    STATUS_UNPAY((byte) 0x01),//1-待支付
+    STATUS_PAY_SUCCESS((byte) 0x02),//2-成功
+    STATUS_PAY_fAIL((byte) 0x03);//3-失败
     public Byte val;
 
     StatusEnum(Byte val) {
