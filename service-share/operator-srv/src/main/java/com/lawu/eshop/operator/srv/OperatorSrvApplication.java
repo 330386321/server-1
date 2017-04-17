94e49d1a9b2ddbeb1ccd9aec942524077b779c14
sunlinqing <linqing.sun@lovelawu.com>
2017-04-17 19:38:40 +0800

    chore(service-share): add operator-srv

---
+++ service-share/operator-srv/src/main/java/com/lawu/eshop/operator/srv/OperatorSrvApplication.java
@@ -0,0 +1,27 @@
+package com.lawu.eshop.operator.srv;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 商城服务启动类
+ * @author Sunny
+ * @date 2017/3/10
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan({"com.lawu.eshop"})
+public class OperatorSrvApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(OperatorSrvApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("operator-srv is starting");
+        SpringApplication.run(OperatorSrvApplication.class, args);
+        logger.info("operator-srv is started");
+    }
+}
