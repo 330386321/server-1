f074786467405cb5fb6d505f16cb02360db13169
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-04-18 07:15:18 +0800

    feat(order-srv|mall-srv):自动提醒卖家发货

---
+++ dto/order-dto/src/main/java/com/lawu/eshop/order/constants/PayOrderStatusEnum.java
@@ -0,0 +1,27 @@
+package com.lawu.eshop.order.constants;
+
+/**
+ * @author zhangyong
+ * @date 2017/4/11.
+ */
+public enum PayOrderStatusEnum {
+    STATUS_DEL((byte) 0x00),//0：删除
+    STATUS_UNPAY((byte) 0x01),//1-待支付
+    STATUS_PAY_SUCCESS((byte) 0x02),//2-成功
+    STATUS_PAY_fAIL((byte) 0x03);//3-失败
+    public Byte val;
+
+    PayOrderStatusEnum(Byte val) {
+        this.val = val;
+    }
+
+    public static PayOrderStatusEnum getEnum(Byte val) {
+        PayOrderStatusEnum[] values = PayOrderStatusEnum.values();
+        for (PayOrderStatusEnum object : values) {
+            if (object.val.equals(val)) {
+                return object;
+            }
+        }
+        return null;
+    }
+}
