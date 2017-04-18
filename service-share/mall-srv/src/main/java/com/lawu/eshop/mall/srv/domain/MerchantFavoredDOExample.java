436bc9af7a3d9f79a79033ad4555b37cd07eba1d
zhangyong <yong.zhang@lovelawu.com>
2017-04-18 11:47:14 +0800

    refactor:修改买单有效期字段类型

--- service-share/mall-srv/src/main/java/com/lawu/eshop/mall/srv/domain/MerchantFavoredDOExample.java
+++ service-share/mall-srv/src/main/java/com/lawu/eshop/mall/srv/domain/MerchantFavoredDOExample.java
@@ -3,6 +3,7 @@ package com.lawu.eshop.mall.srv.domain;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 public class MerchantFavoredDOExample {
@@ -194,6 +195,32 @@ public class MerchantFavoredDOExample {
                 throw new RuntimeException("Between values for " + property + " cannot be null");
             }
             criteria.add(new Criterion(condition, value1, value2));
+        }
+
+        protected void addCriterionForJDBCDate(String condition, Date value, String property) {
+            if (value == null) {
+                throw new RuntimeException("Value for " + property + " cannot be null");
+            }
+            addCriterion(condition, new java.sql.Date(value.getTime()), property);
+        }
+
+        protected void addCriterionForJDBCDate(String condition, List<Date> values, String property) {
+            if (values == null || values.size() == 0) {
+                throw new RuntimeException("Value list for " + property + " cannot be null or empty");
+            }
+            List<java.sql.Date> dateList = new ArrayList<java.sql.Date>();
+            Iterator<Date> iter = values.iterator();
+            while (iter.hasNext()) {
+                dateList.add(new java.sql.Date(iter.next().getTime()));
+            }
+            addCriterion(condition, dateList, property);
+        }
+
+        protected void addCriterionForJDBCDate(String condition, Date value1, Date value2, String property) {
+            if (value1 == null || value2 == null) {
+                throw new RuntimeException("Between values for " + property + " cannot be null");
+            }
+            addCriterion(condition, new java.sql.Date(value1.getTime()), new java.sql.Date(value2.getTime()), property);
         }
 
         public Criteria andIdIsNull() {
@@ -777,52 +804,52 @@ public class MerchantFavoredDOExample {
         }
 
         public Criteria andEntireBeginTimeEqualTo(Date value) {
-            addCriterion("entire_begin_time =", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time =", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeNotEqualTo(Date value) {
-            addCriterion("entire_begin_time <>", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time <>", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeGreaterThan(Date value) {
-            addCriterion("entire_begin_time >", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time >", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeGreaterThanOrEqualTo(Date value) {
-            addCriterion("entire_begin_time >=", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time >=", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeLessThan(Date value) {
-            addCriterion("entire_begin_time <", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time <", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeLessThanOrEqualTo(Date value) {
-            addCriterion("entire_begin_time <=", value, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time <=", value, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeIn(List<Date> values) {
-            addCriterion("entire_begin_time in", values, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time in", values, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeNotIn(List<Date> values) {
-            addCriterion("entire_begin_time not in", values, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time not in", values, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeBetween(Date value1, Date value2) {
-            addCriterion("entire_begin_time between", value1, value2, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time between", value1, value2, "entireBeginTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireBeginTimeNotBetween(Date value1, Date value2) {
-            addCriterion("entire_begin_time not between", value1, value2, "entireBeginTime");
+            addCriterionForJDBCDate("entire_begin_time not between", value1, value2, "entireBeginTime");
             return (Criteria) this;
         }
 
@@ -837,52 +864,52 @@ public class MerchantFavoredDOExample {
         }
 
         public Criteria andEntireEndTimeEqualTo(Date value) {
-            addCriterion("entire_end_time =", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time =", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeNotEqualTo(Date value) {
-            addCriterion("entire_end_time <>", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time <>", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeGreaterThan(Date value) {
-            addCriterion("entire_end_time >", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time >", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeGreaterThanOrEqualTo(Date value) {
-            addCriterion("entire_end_time >=", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time >=", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeLessThan(Date value) {
-            addCriterion("entire_end_time <", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time <", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeLessThanOrEqualTo(Date value) {
-            addCriterion("entire_end_time <=", value, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time <=", value, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeIn(List<Date> values) {
-            addCriterion("entire_end_time in", values, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time in", values, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeNotIn(List<Date> values) {
-            addCriterion("entire_end_time not in", values, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time not in", values, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeBetween(Date value1, Date value2) {
-            addCriterion("entire_end_time between", value1, value2, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time between", value1, value2, "entireEndTime");
             return (Criteria) this;
         }
 
         public Criteria andEntireEndTimeNotBetween(Date value1, Date value2) {
-            addCriterion("entire_end_time not between", value1, value2, "entireEndTime");
+            addCriterionForJDBCDate("entire_end_time not between", value1, value2, "entireEndTime");
             return (Criteria) this;
         }
 
