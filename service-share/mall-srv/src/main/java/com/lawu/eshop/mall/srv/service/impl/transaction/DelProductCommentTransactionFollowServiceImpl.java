7a958e0ec9c5e701be2e85521db543ac243b2f03
yangqinghua <yangqinghua39@163.com>
2017-04-18 13:37:41 +0800

    删除商品型号时删除评价

--- service-share/mall-srv/src/main/java/com/lawu/eshop/mall/srv/service/impl/transaction/DelProductCommentTransactionFollowServiceImpl.java
+++ service-share/mall-srv/src/main/java/com/lawu/eshop/mall/srv/service/impl/transaction/DelProductCommentTransactionFollowServiceImpl.java
@@ -1,17 +1,19 @@
 package com.lawu.eshop.mall.srv.service.impl.transaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.lawu.eshop.compensating.transaction.Reply;
 import com.lawu.eshop.compensating.transaction.annotation.CompensatingTransactionFollow;
 import com.lawu.eshop.compensating.transaction.impl.AbstractTransactionFollowService;
+import com.lawu.eshop.mall.srv.service.CommentProductService;
 import com.lawu.eshop.mq.constants.MqConstant;
 import com.lawu.eshop.mq.dto.product.DelProductCommentNotification;
-
-import org.springframework.stereotype.Service;
 
 /**
  * 
  * <p>
- * Description: 
+ * Description: 删除商品型号时，发送消息删除商品型号下的评价
  * </p>
  * @author Yangqh
  * @date 2017年4月18日 下午12:55:22
@@ -21,11 +23,13 @@ import org.springframework.stereotype.Service;
 @CompensatingTransactionFollow(topic = MqConstant.TOPIC_PRODUCT_SRV, tags = MqConstant.TAG_DEL_COMMENT)
 public class DelProductCommentTransactionFollowServiceImpl extends AbstractTransactionFollowService<DelProductCommentNotification, Reply> {
    
+	@Autowired
+	private CommentProductService commentProductService;
 
     @Override
     public Reply execute(DelProductCommentNotification notification) {
-        Long orderId = notification.getProductModelId();
-       
+        Long productModelId = notification.getProductModelId();
+        commentProductService.delCommentByProductId(productModelId);
         return new Reply();
     }
 
