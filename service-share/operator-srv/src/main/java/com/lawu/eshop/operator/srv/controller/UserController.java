05cee3ba434692f06f2e4f7a0628c11787aadb41
zhangyong <yong.zhang@lovelawu.com>
2017-04-19 17:46:36 +0800

    feat:运营平台用户接口

---
+++ service-share/operator-srv/src/main/java/com/lawu/eshop/operator/srv/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.lawu.eshop.operator.srv.controller;
+
+import com.lawu.eshop.framework.web.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangyong
+ * @date 2017/4/19.
+ */
+@RestController
+@RequestMapping(value = "user")
+public class UserController extends BaseController{
+
+
+}
