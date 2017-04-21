bb78bebcf023f546debd8bc91d27fc1b099e725a
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-04-21 16:34:33 +0800

    refactor:广告接口修改

--- service-share/ad-srv/src/main/java/com/lawu/eshop/ad/srv/GenMain.java
+++ service-share/ad-srv/src/main/java/com/lawu/eshop/ad/srv/GenMain.java
@@ -1,18 +1,5 @@
 package com.lawu.eshop.ad.srv;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.mybatis.generator.api.MyBatisGenerator;
-import org.mybatis.generator.config.Configuration;
-import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.InvalidConfigurationException;
-import org.mybatis.generator.exception.XMLParserException;
-import org.mybatis.generator.internal.DefaultShellCallback;
-
 public class GenMain {
 	/*public static void main(String[] args) {
 		List<String> warnings = new ArrayList<String>();
