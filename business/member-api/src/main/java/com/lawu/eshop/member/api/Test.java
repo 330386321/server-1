6d5e0374d6015c6302fbca278ccfd9432f100428
sunlinqing <linqing.sun@lovelawu.com>
2017-04-21 19:22:18 +0800

    test(member-api): test-main注释

--- business/member-api/src/main/java/com/lawu/eshop/member/api/Test.java
+++ business/member-api/src/main/java/com/lawu/eshop/member/api/Test.java
@@ -4,11 +4,11 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 public class Test {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		String s = "%25+&sd%20&p2=%E4%B8%AD%E6%96%87";
 //		s = URLEncoder.encode(s);
 //		System.out.println(s);
 		s = URLDecoder.decode(s);
 		System.out.println(s);
-	}
+	}*/
 }
