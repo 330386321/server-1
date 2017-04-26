305c406d49263466a48994a4b4d0f9d8bcb96405
meishuquan <shuquan.mei@lovelawu.com>
2017-04-26 16:58:39 +0800

    refactor: 重构要购物首页精品分类

--- business/member-api/src/main/java/com/lawu/eshop/member/api/controller/ProductSolrController.java
+++ business/member-api/src/main/java/com/lawu/eshop/member/api/controller/ProductSolrController.java
@@ -157,9 +157,9 @@ public class ProductSolrController extends BaseController {
                     productSearchDTO.setProductId(adPlatformDTO.getProductId());
                     productSearchDTO.setFeatureImage(adPlatformDTO.getMediaUrl());
                     productSearchDTO.setName(adPlatformDTO.getTitle());
-                    productSearchDTO.setSalesVolume(productInfoDTOResult.getModel().getTotalSales());
-                    productSearchDTO.setOriginalPrice(Double.valueOf(productInfoDTOResult.getModel().getPriceMax()));
-                    productSearchDTO.setPrice(Double.valueOf(productInfoDTOResult.getModel().getPriceMin()));
+                    productSearchDTO.setSalesVolume(productInfoDTOResult.getModel().getTotalSalesVolume());
+                    productSearchDTO.setOriginalPrice(Double.valueOf(productInfoDTOResult.getModel().getMaxPrice()));
+                    productSearchDTO.setPrice(Double.valueOf(productInfoDTOResult.getModel().getMinPrice()));
                     productSearchDTOS.add(productSearchDTO);
                 }
             }
@@ -177,9 +177,9 @@ public class ProductSolrController extends BaseController {
                     productSearchDTO.setProductId(adPlatformDTO.getProductId());
                     productSearchDTO.setFeatureImage(adPlatformDTO.getMediaUrl());
                     productSearchDTO.setName(adPlatformDTO.getTitle());
-                    productSearchDTO.setSalesVolume(productInfoDTOResult.getModel().getTotalSales());
-                    productSearchDTO.setOriginalPrice(Double.valueOf(productInfoDTOResult.getModel().getPriceMax()));
-                    productSearchDTO.setPrice(Double.valueOf(productInfoDTOResult.getModel().getPriceMin()));
+                    productSearchDTO.setSalesVolume(productInfoDTOResult.getModel().getTotalSalesVolume());
+                    productSearchDTO.setOriginalPrice(Double.valueOf(productInfoDTOResult.getModel().getMaxPrice()));
+                    productSearchDTO.setPrice(Double.valueOf(productInfoDTOResult.getModel().getMinPrice()));
                     productSearchDTOS.add(productSearchDTO);
                 }
             }
