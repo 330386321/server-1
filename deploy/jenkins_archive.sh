e13948cb2260e1f452942ad29322011dc2b3b077
sunlinqing <linqing.sun@lovelawu.com>
2017-05-06 15:38:00 +0800

    chore(deploy): Jenkins归档脚本优化，将拷贝步骤移至部署job

--- deploy/jenkins_archive.sh
+++ deploy/jenkins_archive.sh
@@ -1,36 +1,26 @@
 #!/bin/sh
 
-DEPLOY_READY_ROOT_DIR=$1
+# 打包任务的工作空间，自定义参数
+PACKAGE_WORK_SPACE=$1
+
+# 存放目录，当前工作空间
+DEPLOY_READY_ROOT_DIR=${WORKSPACE}
 
 DEPLOY_READY_FOLDER=deploy
 
 # 该目录永远为最新的部署包存放目录
 DEPLOY_READY_DIR=${DEPLOY_READY_ROOT_DIR}/${DEPLOY_READY_FOLDER}
 
-# 当前Jenkins工作空间
-WORK_SPACE=${WORKSPACE}
 
-# 目标目录是否存在
-if [ ! -e ${DEPLOY_READY_ROOT_DIR} ]
+if [ ! -e ${DEPLOY_READY_DIR} ]
 then
-    echo "[ERR] ${DEPLOY_READY_ROOT_DIR} is not exist!"
-    exit
+    # 创建当前部署包存放文件夹
+    mkdir ${DEPLOY_READY_DIR}
 fi
 
-if [ -e ${DEPLOY_READY_DIR} ]
-then
-    # 当前日期
-    CUR_DATE=`date +%Y%m%d-%H%M%S`
+# 删除可能存在的旧文件
+rm -fr ${DEPLOY_READY_DIR}/*
 
-    # 备份上一次的文件（加上当前日期）
-    cd ${DEPLOY_READY_ROOT_DIR}
-    rm -fr ${DEPLOY_READY_FOLDER}
-    #mv ${DEPLOY_READY_FOLDER} ${DEPLOY_READY_FOLDER}${CUR_DATE}
-fi
-
-
-# 创建当前部署包存放文件夹
-mkdir ${DEPLOY_READY_DIR}
 
 
 # 拷贝部署包
@@ -44,7 +34,7 @@ function cpJar() {
     mkdir "${DEPLOY_READY_DIR}/${jarName}"
 
     # 从Jenkins工作空间拷贝文件
-    cp "${WORK_SPACE}/${jarType}/${jarName}/target/${jarName}-1.0-SNAPSHOT.jar" "${DEPLOY_READY_DIR}/${jarName}/"
+    cp "${PACKAGE_WORK_SPACE}/${jarType}/${jarName}/target/${jarName}-1.0-SNAPSHOT.jar" "${DEPLOY_READY_DIR}/${jarName}/"
 }
 
 # business
