7a1c6652cdf2d3fe3bb4e798dbec8d51ab483dc5
meishuquan <shuquan.mei@lovelawu.com>
2017-05-09 12:50:50 +0800

    refactor: 粉丝来源添加关注店铺枚举类型

--- dto/user-dto/src/main/java/com/lawu/eshop/user/constants/FansMerchantChannelEnum.java
+++ dto/user-dto/src/main/java/com/lawu/eshop/user/constants/FansMerchantChannelEnum.java
@@ -10,12 +10,13 @@ package com.lawu.eshop.user.constants;
  *
  */
 public enum FansMerchantChannelEnum {
-	//(1-注册|2-邀请|3-买单消费|4-订单消费|5-抢红包)
+	//(1-注册|2-邀请|3-买单消费|4-订单消费|5-抢红包|6-关注店铺)
     REGISTER((byte) 0x01,"注册"),
     INVITE((byte) 0x02,"邀请"),
     PAY((byte) 0x03,"买单消费"),
     ORDER_PAY((byte) 0x04,"订单消费"),
-    REDPACKET((byte) 0x05,"抢红包");
+    REDPACKET((byte) 0x05,"抢红包"),
+    FOLLOW((byte) 0x06,"关注");
 	
     private Byte value;
     private String name;
