5a366063e54d4b80d66724243526400704a1f6ce
sunlinqing <linqing.sun@lovelawu.com>
2017-05-09 16:39:54 +0800

    feat(operator-api): 新增异常统一处理

---
+++ business/operator-api/src/main/java/com/lawu/eshop/operator/api/OperatorApiExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.lawu.eshop.operator.api;
+
+import com.lawu.eshop.framework.web.BaseController;
+import com.lawu.eshop.framework.web.Result;
+import com.lawu.eshop.framework.web.ResultCode;
+import org.apache.shiro.authz.UnauthenticatedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理
+ * @author Leach
+ * @date 2017/5/9
+ */
+@ControllerAdvice
+public class OperatorApiExceptionHandler extends BaseController {
+
+
+    private Logger logger = LoggerFactory.getLogger(OperatorApiExceptionHandler.class);
+
+    @ExceptionHandler(value = UnauthenticatedException.class)
+    @ResponseBody
+    public Result defaultUnauthenticatedHandler(UnauthenticatedException e) throws Exception {
+        return failUnauthorized(ResultCode.USER_NOT_LOGIN);
+    }
+
+
+}
