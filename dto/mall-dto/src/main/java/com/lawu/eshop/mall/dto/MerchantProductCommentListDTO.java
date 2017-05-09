9d6c58532e32a29f588efb5b8985e53f2596d432
zhangyong <yong.zhang@lovelawu.com>
2017-05-09 17:17:53 +0800

    fix:修改时间格式化

--- dto/mall-dto/src/main/java/com/lawu/eshop/mall/dto/MerchantProductCommentListDTO.java
+++ dto/mall-dto/src/main/java/com/lawu/eshop/mall/dto/MerchantProductCommentListDTO.java
@@ -33,7 +33,7 @@ public class MerchantProductCommentListDTO {
      * 评价时间
      */
     @ApiModelProperty(value = "评价时间")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date gmtCreate;
 
     /**
