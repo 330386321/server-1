a18550c4b1121dfaf0ff1c2dc5ba8ebe025e7590
meishuquan <shuquan.mei@lovelawu.com>
2017-05-11 09:54:38 +0800

    docs: 查询行业信息文档更新

--- business/member-api/src/main/java/com/lawu/eshop/member/api/controller/IndustryTypeController.java
+++ business/member-api/src/main/java/com/lawu/eshop/member/api/controller/IndustryTypeController.java
@@ -32,7 +32,7 @@ public class IndustryTypeController extends BaseController {
 
     @Audit(date = "2017-04-12", reviewer = "孙林青")
     @ApiOperation(value = "查询行业", notes = "查询所有行业。 [1100] (梅述全)", httpMethod = "GET")
-    @ApiResponse(code = HttpCode.SC_CREATED, message = "success")
+    @ApiResponse(code = HttpCode.SC_OK, message = "success")
     @RequestMapping(value = "listIndustryType", method = RequestMethod.GET)
     public Result<List<IndustryTypeDTO>> listIndustryType() {
         return industryTypeService.listIndustryType();
@@ -40,7 +40,7 @@ public class IndustryTypeController extends BaseController {
 
     @Audit(date = "2017-05-11", reviewer = "孙林青")
     @ApiOperation(value = "查询父行业下的所有行业", notes = "查询父行业下的所有行业。 [1100] (梅述全)", httpMethod = "GET")
-    @ApiResponse(code = HttpCode.SC_CREATED, message = "success")
+    @ApiResponse(code = HttpCode.SC_OK, message = "success")
     @RequestMapping(value = "listIndustryType/{parentId}", method = RequestMethod.GET)
     public Result<List<IndustryTypeDTO>> listIndustryType(@PathVariable @ApiParam(required = true, value = "父行业ID") Short parentId) {
         return industryTypeService.listIndustryTypeByParentId(parentId);
