493d06431ee9198295df3d30a455613e6046b829
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-05-12 18:19:01 +0800

    feat:或者红包信息

---
+++ service-share/ad-srv/src/main/java/com/lawu/eshop/ad/srv/bo/RedPacketInfoBO.java
@@ -0,0 +1,39 @@
+package com.lawu.eshop.ad.srv.bo;
+
+import java.math.BigDecimal;
+
+public class RedPacketInfoBO {
+	
+	private BigDecimal point;
+	
+	private String logoUrl;
+	
+	private String name;
+
+	public BigDecimal getPoint() {
+		return point;
+	}
+
+	public void setPoint(BigDecimal point) {
+		this.point = point;
+	}
+
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
