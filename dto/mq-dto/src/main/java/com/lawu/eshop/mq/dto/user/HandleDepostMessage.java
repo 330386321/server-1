ffc85aa6269461402c39fe843269c4ad3a4da7bf
zhangyong <yong.zhang@lovelawu.com>
2017-05-16 10:35:13 +0800

    fix:参数序列化

--- dto/mq-dto/src/main/java/com/lawu/eshop/mq/dto/user/HandleDepostMessage.java
+++ dto/mq-dto/src/main/java/com/lawu/eshop/mq/dto/user/HandleDepostMessage.java
@@ -1,11 +1,16 @@
 package com.lawu.eshop.mq.dto.user;
 
 
+import java.io.Serializable;
+
 /**
  * @author zhangyong
  * @date 2017/5/15.
  */
-public class HandleDepostMessage {
+public class HandleDepostMessage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private String userNum;
 
     private MerchantStatusEnum statusEnum;
