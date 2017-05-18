2543da5d610d95012a482250d3d01dfc535beed3
zhangyong <yong.zhang@lovelawu.com>
2017-05-18 11:33:53 +0800

    fix:融云KEY修改

--- service-share/user-srv/src/main/java/com/lawu/eshop/user/srv/rong/constant/RongYunConstant.java
+++ service-share/user-srv/src/main/java/com/lawu/eshop/user/srv/rong/constant/RongYunConstant.java
@@ -11,7 +11,7 @@ public class RongYunConstant {
     //融云app_secret
     public static final String APP_SECRET = "YHvNnTlTj4";//用户APP
     //融云appkey
-    public static final String M_APP_KEY = "p5tvi9dsp4qw4";//商家APP
+    public static final String M_APP_KEY = "pvxdm17jpgoar";//商家APP
     //融云app_secret
-    public static final String M_APP_SECRET = "YHvNnTlTj4";//商家APP
+    public static final String M_APP_SECRET = "eBOT6XwFzm3W";//商家APP
 }
