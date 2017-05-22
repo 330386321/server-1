5ab161884af5d1baeb43a3243b8d9940dbc0d3ca
zhangyong <yong.zhang@lovelawu.com>
2017-05-22 17:42:55 +0800

    feat：融云聊天记录保存(定时下载保存zip文件)

---
+++ business/statistics/src/main/java/com/lawu/eshop/statistics/service/RongYunService.java
@@ -0,0 +1,18 @@
+package com.lawu.eshop.statistics.service;
+
+import com.lawu.eshop.framework.web.Result;
+import com.lawu.eshop.user.dto.RongYunHistoryMessageDTO;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author zhangyong
+ * @date 2017/5/22.
+ */
+@FeignClient(value = "user-srv")
+public interface RongYunService {
+    @RequestMapping(value = "rongUser/getHistoryMessage", method = RequestMethod.POST)
+    public Result<RongYunHistoryMessageDTO> getHistoryMessage(@RequestParam("date") String date);
+}
