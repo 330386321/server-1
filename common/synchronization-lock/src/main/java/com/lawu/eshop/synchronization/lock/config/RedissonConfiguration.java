b90745a03a07444373f004f3c68739a59ba62bc6
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-06-05 17:45:29 +0800

    fix(synchronization-lock):解决redisson与mq架包依赖冲突

--- common/synchronization-lock/src/main/java/com/lawu/eshop/synchronization/lock/config/RedissonConfiguration.java
+++ common/synchronization-lock/src/main/java/com/lawu/eshop/synchronization/lock/config/RedissonConfiguration.java
@@ -6,7 +6,6 @@ import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
 /**
