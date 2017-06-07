b3816f3a6e8924539edfe907a474bb7cf37c20f8
sunlinqing <slq@smp.local>
2017-06-08 00:51:53 +0800

    test(cache-srv): 添加用户token获取单元测试

--- service-sys/cache-srv/src/test/java/com/lawu/eshop/cache/srv/CacheSrvApplicationTest.java
+++ service-sys/cache-srv/src/test/java/com/lawu/eshop/cache/srv/CacheSrvApplicationTest.java
@@ -4,24 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * 用户服务启动类
  * @author Leach
  * @date 2017/3/10
  */
-@EnableDiscoveryClient
-@ComponentScan({"com.lawu.eshop"})
+@ComponentScan(value = {"com.lawu.eshop"}, excludeFilters = {@ComponentScan.Filter(value = {CacheSrvApplication.class}, type = FilterType.ASSIGNABLE_TYPE)})
 @SpringBootApplication
-public class CacheSrvApplication {
+public class CacheSrvApplicationTest {
 
-    private static Logger logger = LoggerFactory.getLogger(CacheSrvApplication.class);
+    private static Logger logger = LoggerFactory.getLogger(CacheSrvApplicationTest.class);
 
     public static void main(String[] args) {
         logger.info("cache-srv is starting");
-        SpringApplication.run(CacheSrvApplication.class, args);
+        SpringApplication.run(CacheSrvApplicationTest.class, args);
         logger.info("cache-srv is started");
     }
 }
