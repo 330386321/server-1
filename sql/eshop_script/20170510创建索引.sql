d3f3ec135a1dc68fda88780d37e1816a4727392a
sunlinqing <linqing_sun@lovelawu.com>
2017-06-19 11:25:27 +0800

    refactor(sql): 数据库索引整理

--- sql/eshop_script/20170510创建索引.sql
+++ sql/eshop_script/20170510创建索引.sql
@@ -7,9 +7,9 @@ alter table ad_region add KEY idx_ad_id(ad_id);
 
 alter table favorite_ad add KEY idx_member_id(member_id);
 
-alter table member_ad_record add KEY idx_member_id(member_id);
 alter table member_ad_record add KEY idx_member_num(member_num);
 alter table member_ad_record add KEY idx_ad_id(ad_id);
+alter table member_ad_record add KEY idx_member_id_click_date(member_id, click_date);
 
 alter table point_pool add KEY idx_member_id(member_id);
 alter table point_pool add KEY idx_member_num(member_num);
@@ -18,7 +18,7 @@ alter table point_pool add KEY idx_ad_id_status(ad_id, status);
 alter table transaction_record add UNIQUE KEY uk_relate_id_type(relate_id, type);
 
 -- mall
-alter table comment_image add KEY idx_comment_id_status(comment_id, status);
+alter table comment_image add KEY idx_comment_id_type_status(comment_id, type, status);
 
 alter table comment_merchant add KEY idx_merchant_id_status(merchant_id, status);
 
@@ -28,29 +28,23 @@ alter table express_company add UNIQUE KEY uk_code(code);
 
 alter table merchant_favored add KEY idx_merchant_id_status(merchant_id, status);
 
-alter table message add KEY idx_user_num(user_num);
 alter table message add KEY idx_user_num_status(user_num, status);
 
 alter table sms_record add KEY idx_mobile(mobile);
 
-alter table verify_code add KEY idx_mobile(mobile);
 alter table verify_code add KEY idx_mobile_purpose(mobile, purpose);
 
 alter table transaction_record add UNIQUE KEY uk_relate_id_type(relate_id, type);
 
 -- order
 alter table pay_order add UNIQUE KEY uk_order_num(order_num);
-alter table pay_order add KEY idx_member_id(member_id);
 alter table pay_order add KEY idx_member_id_status(member_id, status);
-alter table pay_order add KEY idx_merchant_id(merchant_id);
 alter table pay_order add KEY idx_merchant_id_status(merchant_id, status);
 
 alter table shopping_cart add KEY idx_member_id(member_id);
 
 alter table shopping_order add UNIQUE KEY uk_order_num(order_num);
-alter table shopping_order add KEY idx_member_id(member_id);
 alter table shopping_order add KEY idx_member_id_status_order_status(member_id, status, order_status);
-alter table shopping_order add KEY idx_merchant_id(merchant_id);
 alter table shopping_order add KEY idx_merchant_id_status_order_status(merchant_id, status, order_status);
 
 alter table shopping_order_item add KEY idx_shopping_order_id(shopping_order_id);
@@ -62,13 +56,10 @@ alter table transaction_record add UNIQUE KEY uk_relate_id_type(relate_id, type)
 -- product
 alter table favorite_product add KEY idx_member_id(member_id);
 
-alter table product add KEY idx_merchant_id(merchant_id);
-alter table product add KEY idx_merchant_id_status(merchant_id, status);
 alter table product add KEY idx_merchant_id_status_sales(merchant_id, status, total_sales_volume);
 
 alter table product_category add KEY idx_parent_id(parent_id);
 
-alter table product_image add KEY idx_product_id_status_img_type(product_id, status, img_type);
 alter table product_image add KEY idx_product_id_status_img_type_sortid(product_id, status, img_type, sortid);
 
 alter table product_model add KEY idx_product_id_status(product_id, status);
@@ -85,21 +76,17 @@ alter table business_deposit add KEY idx_user_num(user_num);
 
 alter table fans_invite_detail add KEY idx_merchant_id(merchant_id);
 
-alter table freeze add KEY idx_user_num(user_num);
 alter table freeze add KEY idx_user_num_status(user_num, status);
 
 alter table love_detail add UNIQUE KEY uk_love_num(love_num);
-alter table love_detail add KEY idx_user_num(user_num);
 alter table love_detail add KEY idx_user_num_gmt_create(user_num, gmt_create);
 
 alter table point_detail add UNIQUE KEY uk_point_num(point_num);
-alter table point_detail add KEY idx_user_num(user_num);
 alter table point_detail add KEY idx_user_num_gmt_create(user_num, gmt_create);
 
 alter table recharge add KEY idx_user_num(user_num);
 
 alter table transaction_detail add UNIQUE KEY uk_transaction_num(transaction_num);
-alter table transaction_detail add KEY idx_user_num(user_num);
 alter table transaction_detail add KEY idx_user_num_gmt_create(user_num, gmt_create);
 
 alter table withdraw_cash add KEY idx_user_num(user_num);
@@ -115,7 +102,6 @@ alter table fans_merchant add KEY idx_member_id(member_id);
 alter table favorite_merchant add KEY idx_member_id(member_id);
 
 alter table invite_relation add KEY idx_user_num(user_num);
-alter table invite_relation add KEY idx_invite_user_num(invite_user_num);
 alter table invite_relation add KEY idx_invite_user_num_depth(invite_user_num, depth);
 
 alter table merchant add UNIQUE KEY uk_account(account);
