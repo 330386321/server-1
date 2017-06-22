a5ec643554650e08c6db9c5f5316b9ab4a45cce0
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-06-22 11:03:58 +0800

    refactor:删除无用代码

--- service-sys/cache-srv/src/main/java/com/lawu/eshop/cache/srv/service/AdService.java
+++
@@ -1,5 +0,0 @@
-package com.lawu.eshop.cache.srv.service;
-
-public interface AdService {
-	
-}
--- service-sys/cache-srv/src/main/java/com/lawu/eshop/cache/srv/service/impl/AdServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.lawu.eshop.cache.srv.service.impl;
-
-import com.lawu.eshop.cache.srv.service.AdService;
-
-public class AdServiceImpl implements AdService{
-	
-	
-}
