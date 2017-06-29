1dc468f02493849d7314283056c1a1431ff3718a
sunlinqing <linqing_sun@lovelawu.com>
2017-06-29 15:57:01 +0800

    docs(sql): 用户访问记录表 新增地区信息

--- sql/eshop_statistic/20170628新建用户访问记录表.sql
+++ sql/eshop_statistic/20170628新建用户访问记录表.sql
@@ -3,7 +3,9 @@ CREATE TABLE `user_visit_record` (
   `user_num` varchar(19) NOT NULL COMMENT '用户编号',
   `account` varchar(20) NOT NULL COMMENT '用户账号',
   `user_type` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '用户类型，1是商家，2是会员',
-  `visit_count` int(8) unsigned COMMENT '访问次数',
+  `visit_count` int(8) unsigned DEFAULT NULL COMMENT '访问次数',
+  `city_id` int(8) unsigned DEFAULT '0' COMMENT '市级区域ID',
+  `city_name` varchar(50) DEFAULT '' COMMENT '市级名称',
   `gmt_create` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) COMMENT='用户访问记录';
