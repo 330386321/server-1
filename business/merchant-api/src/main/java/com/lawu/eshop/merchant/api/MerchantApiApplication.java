f80d82aabc80348ea309ab4db09dd770354930ba
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-06-30 11:35:46 +0800

    refactor(feamework-web):XssFilter扫描自动注册改为手动注册

--- business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/MerchantApiApplication.java
+++ business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/MerchantApiApplication.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,7 +20,6 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ImportResource(locations={"classpath:spring.xml"})
 @ComponentScan(basePackages={"com.lawu.eshop"})
-@ServletComponentScan(basePackages = {"com.lawu.eshop"})
 public class MerchantApiApplication {
 
     private static Logger logger = LoggerFactory.getLogger(MerchantApiApplication.class);
