671184a873b077df574325ef8000a8dd02246674
zhangyong <yong.zhang@lovelawu.com>
2017-06-30 11:40:34 +0800

    feat:活跃用户统计（按地区）-接口、定时任务(统计一个月地区活跃用户)

---
+++ business/jobs/src/main/java/com/lawu/eshop/jobs/impl/user/UserActiveAreaMonthJob.java
@@ -0,0 +1,15 @@
+package com.lawu.eshop.jobs.impl.user;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+
+/**
+ * @author zhangyong
+ * @date 2017/6/30.
+ */
+public class UserActiveAreaMonthJob implements SimpleJob {
+    @Override
+    public void execute(ShardingContext shardingContext) {
+
+    }
+}
