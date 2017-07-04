b309830d594963b60ba35a7f373e6f3ad63a017b
sunlinqing <linqing_sun@lovelawu.com>
2017-07-04 15:57:47 +0800

    docs(sql): 修正用户访问记录表用户类型字段注释

--- sql/eshop_statistic/20170628新建用户访问记录表.sql
+++ sql/eshop_statistic/20170628新建用户访问记录表.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user_visit_record` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `user_num` varchar(19) NOT NULL COMMENT '用户编号',
   `account` varchar(20) NOT NULL COMMENT '用户账号',
-  `user_type` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '用户类型，1是商家，2是会员',
+  `user_type` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '用户类型，1是会员，2是商家',
   `visit_count` int(8) unsigned DEFAULT NULL COMMENT '访问次数',
   `city_id` int(8) unsigned DEFAULT '0' COMMENT '市级区域ID',
   `city_name` varchar(50) DEFAULT '' COMMENT '市级名称',
