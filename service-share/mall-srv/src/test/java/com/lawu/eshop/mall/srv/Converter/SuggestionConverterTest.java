d592b1156317c31d59696638d99cdad602d81e8a
zhangyong <yong.zhang@lovelawu.com>
2017-07-19 17:48:27 +0800

    feat:增加SuggestionConverterTest单元测试

--- service-share/mall-srv/src/test/java/com/lawu/eshop/mall/srv/Converter/SuggestionConverterTest.java
+++ service-share/mall-srv/src/test/java/com/lawu/eshop/mall/srv/Converter/SuggestionConverterTest.java
@@ -3,6 +3,7 @@ package com.lawu.eshop.mall.srv.Converter;
 import com.lawu.eshop.mall.constants.SuggestionClientType;
 import com.lawu.eshop.mall.constants.SuggestionUserType;
 import com.lawu.eshop.mall.dto.SuggestionDTO;
+import com.lawu.eshop.mall.param.SuggestionParam;
 import com.lawu.eshop.mall.srv.bo.SuggestionBO;
 import com.lawu.eshop.mall.srv.converter.SuggestionConverter;
 import com.lawu.eshop.mall.srv.domain.SuggestionDO;
@@ -84,5 +85,14 @@ public class SuggestionConverterTest {
         bos.add(bo);
         List<SuggestionDTO> list = SuggestionConverter.convertDTOS(bos);
         Assert.assertEquals(bos.get(0).getContent(), list.get(0).getContent());
+    }
+
+    @Test
+    public void convertDO() {
+        SuggestionParam param = new SuggestionParam();
+        param.setClientType(SuggestionClientType.ANDROID);
+        param.setContent("test");
+        SuggestionDO suggestionDO = SuggestionConverter.convert("M123456", param);
+        Assert.assertEquals(param.getContent(), suggestionDO.getContent());
     }
 }
---
+++ service-share/mall-srv/src/test/java/com/lawu/eshop/mall/srv/Converter/VerifyCodeConverterTest.java
@@ -0,0 +1,45 @@
+package com.lawu.eshop.mall.srv.Converter;
+
+import com.lawu.eshop.mall.dto.VerifyCodeDTO;
+import com.lawu.eshop.mall.srv.bo.VerifyCodeBO;
+import com.lawu.eshop.mall.srv.converter.VerifyCodeConverter;
+import com.lawu.eshop.mall.srv.domain.VerifyCodeDO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+/**
+ * @author zhangyong
+ * @date 2017/7/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-test.xml"})
+public class VerifyCodeConverterTest {
+
+    @Test
+    public void convertBO() {
+        VerifyCodeDO verifyCodeDO = new VerifyCodeDO();
+        verifyCodeDO.setId(1L);
+        verifyCodeDO.setMobile("123");
+        verifyCodeDO.setCode("1234");
+        verifyCodeDO.setGmtCreate(new Date());
+        VerifyCodeBO verifyCodeBO = VerifyCodeConverter.convertBO(verifyCodeDO);
+        Assert.assertEquals(verifyCodeDO.getCode(), verifyCodeBO.getCode());
+    }
+
+    @Test
+    public void convertDTO() {
+        VerifyCodeBO verifyCodeBO = new VerifyCodeBO();
+        verifyCodeBO.setId(1L);
+        verifyCodeBO.setMobile("123");
+        verifyCodeBO.setCode("1234");
+        verifyCodeBO.setGmtCreate(new Date());
+        VerifyCodeDTO verifyCodeDTO = VerifyCodeConverter.convertDTO(verifyCodeBO);
+        Assert.assertEquals(verifyCodeBO.getCode(), verifyCodeDTO.getCode());
+
+    }
+}
