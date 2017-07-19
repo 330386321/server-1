2530e4d7a2a3c484b4621764982ebf2e11f4135f
zhangyong <yong.zhang@lovelawu.com>
2017-07-19 17:30:25 +0800

    feat:增加SearchWordConverterTest单元测试

---
+++ service-share/mall-srv/src/test/java/com/lawu/eshop/mall/srv/Converter/SearchWordConverterTest.java
@@ -0,0 +1,45 @@
+package com.lawu.eshop.mall.srv.Converter;
+
+import com.lawu.eshop.mall.constants.SearchWordTypeEnum;
+import com.lawu.eshop.mall.dto.SearchWordDTO;
+import com.lawu.eshop.mall.srv.bo.SearchWordBO;
+import com.lawu.eshop.mall.srv.converter.SearchWordConverter;
+import com.lawu.eshop.mall.srv.domain.SearchWordDO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+/**
+ * @author zhangyong
+ * @date 2017/7/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-test.xml"})
+public class SearchWordConverterTest {
+
+    @Test
+    public void convertBO() {
+        SearchWordDO searchWordDO = new SearchWordDO();
+        searchWordDO.setId(1L);
+        searchWordDO.setWord("KEY");
+        searchWordDO.setType(SearchWordTypeEnum.WORD_TYPE_PRODUCT.val);
+        searchWordDO.setGmtCreate(new Date());
+        SearchWordBO searchWordBO = SearchWordConverter.convertBO(searchWordDO);
+        Assert.assertEquals(searchWordDO.getWord(), searchWordBO.getWord());
+    }
+
+    @Test
+    public void convertDTO() {
+        SearchWordBO searchWordBO = new SearchWordBO();
+        searchWordBO.setId(1L);
+        searchWordBO.setWord("KEY");
+        searchWordBO.setType(SearchWordTypeEnum.WORD_TYPE_PRODUCT.val);
+        searchWordBO.setGmtCreate(new Date());
+        SearchWordDTO searchWordDTO = SearchWordConverter.convertDTO(searchWordBO);
+        Assert.assertEquals(searchWordBO.getWord(), searchWordDTO.getWord());
+    }
+}
