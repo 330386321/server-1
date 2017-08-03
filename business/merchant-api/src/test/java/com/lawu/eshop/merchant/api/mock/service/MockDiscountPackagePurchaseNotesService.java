1bebe0c49f23e2daf38c942cb0f267d2f06c594e
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-08-03 18:04:19 +0800

    fix(merchant-api):解决冲突

--- business/merchant-api/src/test/java/com/lawu/eshop/merchant/api/mock/service/MockDiscountPackagePurchaseNotesService.java
+++ business/merchant-api/src/test/java/com/lawu/eshop/merchant/api/mock/service/MockDiscountPackagePurchaseNotesService.java
@@ -1,13 +1,13 @@
 package com.lawu.eshop.merchant.api.mock.service;
 
-<<<<<<< Upstream, based on branch 'develop' of git@192.168.1.24:eshop/server.git
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.lawu.eshop.framework.web.BaseController;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.mall.dto.DiscountPackagePurchaseNotesDTO;
 import com.lawu.eshop.merchant.api.service.DiscountPackagePurchaseNotesService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * @author meishuquan
@@ -19,26 +19,4 @@ public class MockDiscountPackagePurchaseNotesService extends BaseController impl
     public Result<List<DiscountPackagePurchaseNotesDTO>> list() {
         return successGet();
     }
-=======
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.lawu.eshop.framework.web.BaseController;
-import com.lawu.eshop.framework.web.Result;
-import com.lawu.eshop.mall.dto.DiscountPackagePurchaseNotesDTO;
-import com.lawu.eshop.merchant.api.service.DiscountPackagePurchaseNotesService;
-
-/**
- * @author meishuquan
- * @date 2017/7/24.
- */
-@Service
-public class MockDiscountPackagePurchaseNotesService extends BaseController implements DiscountPackagePurchaseNotesService {
-
-	@Override
-	public Result<List<DiscountPackagePurchaseNotesDTO>> list() {
-		return successGet();
-	}
->>>>>>> db30afd feat(merchant-api):修改更新和保存优惠套餐接口的上传方式
 }
