83ce2ef1f776e24676824d7359c661369042ec87
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-08-08 17:32:24 +0800

    refactor:运营平台广告位添加修改

--- business/operator-api/src/main/java/com/lawu/eshop/operator/api/controller/AdPlatformController.java
+++ business/operator-api/src/main/java/com/lawu/eshop/operator/api/controller/AdPlatformController.java
@@ -1,11 +1,7 @@
 package com.lawu.eshop.operator.api.controller;
 
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawu.eshop.ad.dto.AdPlatformOperatorDTO;
@@ -27,7 +22,6 @@ import com.lawu.eshop.framework.web.BaseController;
 import com.lawu.eshop.framework.web.HttpCode;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.framework.web.annotation.PageBody;
-import com.lawu.eshop.framework.web.constants.FileDirConstant;
 import com.lawu.eshop.operator.api.OperatorApiConfig;
 import com.lawu.eshop.operator.api.service.AdPlatformService;
 import com.lawu.eshop.operator.api.service.AdService;
@@ -45,7 +39,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
-import util.UploadFileUtil;
 
 /**
  * 描述：广告管理
@@ -137,7 +130,7 @@ public class AdPlatformController extends BaseController {
     
     @ApiOperation(value = "添加广告", notes = "添加广告[1011|1015|1010]（张荣成）", httpMethod = "POST")
     @ApiResponse(code = HttpCode.SC_CREATED, message = "success")
-    //@RequiresPermissions("adPlatForm:add")
+    @RequiresPermissions("adPlatForm:add")
     @RequestMapping(value = "saveAdPlatform", method = RequestMethod.POST)
     public Result saveAdPlatform(@ModelAttribute @ApiParam(required = true, value = "广告信息") AdPlatformParam adPlatform) {
         return adPlatformService.saveAdPlatform(adPlatform);
@@ -155,7 +148,7 @@ public class AdPlatformController extends BaseController {
     
     @ApiOperation(value = "单个广告查询", notes = "单个广告查询[]（张荣成）", httpMethod = "GET")
     @ApiResponse(code = HttpCode.SC_NO_CONTENT, message = "success")
-    //@RequiresAuthentication
+    @RequiresAuthentication
     @RequestMapping(value = "select/{id}", method = RequestMethod.GET)
     public Result<AdPlatformOperatorDTO> setPositon(@PathVariable @ApiParam(required = true, value = "广告id") Long id) {
         return adPlatformService.select(id);
