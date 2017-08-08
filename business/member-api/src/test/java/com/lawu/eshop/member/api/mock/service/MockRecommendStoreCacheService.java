a7284930357338a8a7acec512e6bf2abbf7e12ce
meishuquan <shuquan.mei@lovelawu.com>
2017-08-08 18:24:38 +0800

    test(member-api): member-api优选美食接口单元测试

--- business/member-api/src/test/java/com/lawu/eshop/member/api/mock/service/MockRecommendStoreCacheService.java
+++ business/member-api/src/test/java/com/lawu/eshop/member/api/mock/service/MockRecommendStoreCacheService.java
@@ -1,10 +1,11 @@
 package com.lawu.eshop.member.api.mock.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.lawu.eshop.framework.web.BaseController;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.member.api.service.RecommendStoreCacheService;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class MockRecommendStoreCacheService extends BaseController implements RecommendStoreCacheService {
@@ -16,11 +17,11 @@ public class MockRecommendStoreCacheService extends BaseController implements Re
 
     @Override
     public Result<String> getRecommendFoodConsume(@RequestParam("regionPath") String regionPath) {
-        return successCreated("{\"address\":\"sdfd\",\"areaName\":\"fdfd\",\"averageConsumeAmount\":2,\"averageScore\":3,\"buyNumbers\":1,\"commentsCount\":1,\"discountPackage\":\"dd\",\"distance\":3,\"favoreInfo\":\"33\",\"industryName\":\"#3\",\"latitude\":33,\"longitude\":343,\"merchantId\":1,\"merchantStoreId\":1,\"name\":\"fdf\",\"regionName\":\"fdfd\",\"storePic\":\"fdfd\"}");
+        return successCreated("{\"address\":\"sdfd\",\"areaName\":\"fdfd\",\"averageConsumeAmount\":2,\"averageScore\":3,\"buyNumbers\":1,\"commentsCount\":1,\"discountPackage\":\"dd\",\"distance\":3,\"favoreInfo\":\"33\",\"industryName\":\"#3\",\"latitude\":22,\"longitude\":104,\"merchantId\":1,\"merchantStoreId\":1,\"name\":\"fdf\",\"regionName\":\"fdfd\",\"storePic\":\"fdfd\"}");
     }
 
     @Override
     public Result<String> getRecommendFoodComment(@RequestParam("regionPath") String regionPath) {
-        return successCreated("{\"latitude\":33,\"longitude\":343}");
+        return successCreated("{\"address\":\"sdfd\",\"areaName\":\"fdfd\",\"averageConsumeAmount\":2,\"averageScore\":3,\"buyNumbers\":1,\"commentsCount\":1,\"discountPackage\":\"dd\",\"distance\":3,\"favoreInfo\":\"33\",\"industryName\":\"#3\",\"latitude\":22,\"longitude\":104,\"merchantId\":1,\"merchantStoreId\":1,\"name\":\"fdf\",\"regionName\":\"fdfd\",\"storePic\":\"fdfd\"}");
     }
 }
