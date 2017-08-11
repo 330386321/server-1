66c7f5ec0eb78200538e1faa813774da15075d2e
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-08-11 13:50:58 +0800

    fix(sql):discount_package_content表新增字段脚本执行报错

--- sql/eshop_mall/20170727优惠套餐内容表.sql
+++ sql/eshop_mall/20170727优惠套餐内容表.sql
@@ -1,2 +1,2 @@
 -- 增加小计字段长度
-ALTER TABLE `discount_package_image` MODIFY COLUMN `subtotal` decimal(11,2) unsigned NOT NULL COMMENT '小计' AFTER `unit`;
+ALTER TABLE `discount_package_content` MODIFY COLUMN `subtotal` decimal(11,2) unsigned NOT NULL COMMENT '小计' AFTER `unit`;
