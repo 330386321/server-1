d4d5d366735c49755334dcd03d32cbcfd8e32a18
zhangyong <yong.zhang@lovelawu.com>
2017-08-11 17:57:55 +0800

    fix:修改agent单元测试路径错误

--- service-share/agent-srv/src/test/java/com/lawu/eshop/agent/srv/service/AgentSrvApplicationTest.java
+++ service-share/agent-srv/src/test/java/com/lawu/eshop/agent/srv/service/AgentSrvApplicationTest.java
@@ -1,4 +1,4 @@
-package com.lawu.eshop.agent.srv;
+package com.lawu.eshop.agent.srv.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
