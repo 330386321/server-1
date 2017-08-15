011bb967037d258c811d9f62401731367927211e
zhangyong <yong.zhang@lovelawu.com>
2017-08-15 15:18:30 +0800

    refactor:增加登录权限验证

--- business/agent-api/src/main/java/com/lawu/eshop/agent/api/controller/ReportAreaActiveUserController.java
+++ business/agent-api/src/main/java/com/lawu/eshop/agent/api/controller/ReportAreaActiveUserController.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawu.eshop.agent.api.service.ReportAreaUserActiveService;
+import com.lawu.eshop.authorization.annotation.Authorization;
 import com.lawu.eshop.framework.web.BaseController;
 import com.lawu.eshop.framework.web.HttpCode;
 import com.lawu.eshop.framework.web.Result;
+import com.lawu.eshop.framework.web.constants.UserConstant;
 import com.lawu.eshop.statistics.dto.UserActiveListDTO;
 import com.lawu.eshop.statistics.param.AgentReportParam;
 
@@ -33,15 +36,19 @@ public class ReportAreaActiveUserController extends BaseController{
 
     @ApiOperation(value = "查询活跃用户日统计列表", notes = "查询活跃用户日统计列表（章勇）", httpMethod = "GET")
     @ApiResponse(code = HttpCode.SC_OK, message = "success")
+    @Authorization
     @RequestMapping(value ="getAgentUserActiveListDaily" ,method = RequestMethod.GET)
-    public Result<List<UserActiveListDTO>> getAgentUserActiveListDaily(@ModelAttribute AgentReportParam param){
+    public Result<List<UserActiveListDTO>> getAgentUserActiveListDaily(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token,
+                                                                       @ModelAttribute AgentReportParam param){
         return reportAreaUserActiveService.getAgentUserActiveListDaily(param);
     }
 
     @ApiOperation(value = "查询活跃用户月统计列表", notes = "查询活跃用户月统计列表（章勇）", httpMethod = "GET")
     @ApiResponse(code = HttpCode.SC_OK, message = "success")
-        @RequestMapping(value ="getAgentUserActiveListMonth" ,method = RequestMethod.GET)
-    public Result<List<UserActiveListDTO>> getAgentUserActiveListMonth(@ModelAttribute AgentReportParam param){
+    @Authorization
+    @RequestMapping(value ="getAgentUserActiveListMonth" ,method = RequestMethod.GET)
+    public Result<List<UserActiveListDTO>> getAgentUserActiveListMonth(@RequestHeader(UserConstant.REQ_HEADER_TOKEN) String token,
+                                                                       @ModelAttribute AgentReportParam param){
         return reportAreaUserActiveService.getAgentUserActiveListMonth(param);
     }
 }
