cd67d9b94b697d2e6a1cbffea34c879aaa5f8585
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-08-26 17:58:20 +0800

    fix(ad-srv|property-srv):广告收益清单修改

--- dto/property-dto/src/main/java/com/lawu/eshop/property/param/ReportAdEarningsPointParam.java
+++ dto/property-dto/src/main/java/com/lawu/eshop/property/param/ReportAdEarningsPointParam.java
@@ -1,10 +1,13 @@
 package com.lawu.eshop.property.param;
 
+import com.lawu.eshop.property.constants.LoveTypeEnum;
 import com.lawu.eshop.property.constants.MemberTransactionTypeEnum;
 
 public class ReportAdEarningsPointParam {
 
 	private MemberTransactionTypeEnum  MemberTransactionTypeEnum;
+	
+	private LoveTypeEnum loveTypeEnum;
 	
 	private Long bizId;
 
@@ -22,6 +25,14 @@ public class ReportAdEarningsPointParam {
 
 	public void setBizId(Long bizId) {
 		this.bizId = bizId;
+	}
+
+	public LoveTypeEnum getLoveTypeEnum() {
+		return loveTypeEnum;
+	}
+
+	public void setLoveTypeEnum(LoveTypeEnum loveTypeEnum) {
+		this.loveTypeEnum = loveTypeEnum;
 	}
 
 	
