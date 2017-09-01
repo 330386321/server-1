5cf0759f3088387747578ee579e22237aafabb14
yangqinghua <yangqinghua39@163.com>
2017-09-01 14:50:19 +0800

    fix(mall-dto):解决bug=SER-431

--- dto/mall-dto/src/main/java/com/lawu/eshop/mall/dto/CommentMerchantDTO.java
+++ dto/mall-dto/src/main/java/com/lawu/eshop/mall/dto/CommentMerchantDTO.java
@@ -37,7 +37,7 @@ public class CommentMerchantDTO {
      * 评价时间
      */
     @ApiModelProperty(value = "评价时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private Date gmtCreate;
 
     /**
