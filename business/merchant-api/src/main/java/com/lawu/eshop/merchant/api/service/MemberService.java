71121fd9f0b0c67a041d8dc50f5032da21005855
meishuquan <shuquan.mei@lovelawu.com>
2017-09-06 18:56:32 +0800

    fix(member-api,merchant-api): 注册添加邀请类型字段

--- business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/service/MemberService.java
+++ business/merchant-api/src/main/java/com/lawu/eshop/merchant/api/service/MemberService.java
@@ -1,14 +1,20 @@
 package com.lawu.eshop.merchant.api.service;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.lawu.eshop.framework.core.page.Page;
 import com.lawu.eshop.framework.web.Result;
 import com.lawu.eshop.user.dto.EfriendDTO;
+import com.lawu.eshop.user.dto.MemberDTO;
 import com.lawu.eshop.user.dto.RongYunDTO;
 import com.lawu.eshop.user.dto.UserDTO;
 import com.lawu.eshop.user.dto.VisitUserInfoDTO;
 import com.lawu.eshop.user.param.MemberQuery;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.*;
 
 /**
  * @author zhangyong
@@ -58,6 +64,16 @@ public interface MemberService {
      * @param userNum
      * @return
      */
-    @RequestMapping(value = "findUserAccountAndRegionPathByNum", method = RequestMethod.GET)
+    @RequestMapping(value = "member/findUserAccountAndRegionPathByNum", method = RequestMethod.GET)
     VisitUserInfoDTO findUserAccountAndRegionPathByNum(@PathVariable("userNum") String userNum);
+
+    /**
+     * 根据账号查询会员信息
+     *
+     * @param account 会员账号
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.GET, value = "member/getMember/{account}")
+    Result<MemberDTO> getMemberByAccount(@PathVariable("account") String account);
+
 }
