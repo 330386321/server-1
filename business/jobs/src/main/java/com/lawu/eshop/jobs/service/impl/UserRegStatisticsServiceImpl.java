bc27e73784b546c3fef05d02e6ce9687cd09faf6
meishuquan <shuquan.mei@lovelawu.com>
2017-09-12 17:42:44 +0800

    fix(jobs): 用户区域注册统计取消直辖市县辖数据

--- business/jobs/src/main/java/com/lawu/eshop/jobs/service/impl/UserRegStatisticsServiceImpl.java
+++ business/jobs/src/main/java/com/lawu/eshop/jobs/service/impl/UserRegStatisticsServiceImpl.java
@@ -94,19 +94,6 @@ public class UserRegStatisticsServiceImpl implements UserRegStatisticsService {
             userRegAreaParam.setMerchantEntityCount(merchantEntityResult.getModel());
             userRegAreaParam.setMerchantCount(merchantCommonResult.getModel() + merchantEntityResult.getModel());
             userRegAreaParam.setCityId(regionDTO.getId());
-            if (regionDTO.getId() == 1102) {
-                //北京县辖数据统计到北京市辖
-                userRegAreaParam.setCityId(1101);
-            } else if (regionDTO.getId() == 1202) {
-                //天津县辖数据统计到天津市辖
-                userRegAreaParam.setCityId(1201);
-            } else if (regionDTO.getId() == 3102) {
-                //上海县辖数据统计到上海市辖
-                userRegAreaParam.setCityId(3101);
-            } else if (regionDTO.getId() == 5002) {
-                //重庆县辖数据统计到重庆市辖
-                userRegAreaParam.setCityId(5001);
-            }
             userRegService.updateUserRegArea(userRegAreaParam);
         }
     }
