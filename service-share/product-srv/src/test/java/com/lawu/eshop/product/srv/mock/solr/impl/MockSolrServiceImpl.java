e153cd78a2dc4c95ad206b6073c8cbb38bc75ff3
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-09-19 09:20:27 +0800

    test(product-srv):mock代码冲突

--- service-share/product-srv/src/test/java/com/lawu/eshop/product/srv/mock/solr/impl/MockSolrServiceImpl.java
+++ service-share/product-srv/src/test/java/com/lawu/eshop/product/srv/mock/solr/impl/MockSolrServiceImpl.java
@@ -97,11 +97,6 @@ public class MockSolrServiceImpl implements SolrService {
 		return null;
 	}
 
-	@Override
-	public SolrDocumentList getSolrDocsByQueryPost(SolrQuery query, String solrUrl, String solrCore, Boolean isCloudSolr) {
-		return null;
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
