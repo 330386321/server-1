16151e52f675865e6e0d5f04463e8ee6201ee53b
yangqinghua <yangqinghua39@163.com>
2017-09-26 19:09:06 +0800

    sql:脚本提交

--- sql/v1.3.6/v1.3.6.12/mall_20170926修改门店行业基础数据.sql
+++ sql/v1.3.6/v1.3.6.12/mall_20170926修改门店行业基础数据.sql
@@ -1,10 +1,15 @@
-delete from recommend_product_category;
-INSERT INTO `recommend_product_category` VALUES ('1', '1', '食品', '1', '1', '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('2', '2', '美妆', '1', '2', '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('3', '3', '服装', '1', 3, '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('4', '4', '百货', '1', 4, '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('5', '5', '鞋靴箱包', '1', '5', '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('6', '6', '数码家电', '1', 6, '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('7', '7', '内衣饰品', '1', 7, '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('8', '8', '汽车用品', '1', 8, '2017-04-21 00:04:12');
-INSERT INTO `recommend_product_category` VALUES ('9', '9', '其他', '1', 9, '2017-04-21 00:04:12');
+update industry_type set name='酒店住宿' where id=12;
+update industry_type set name='旅游摄影' where id=16;
+update industry_type set name='医疗健康' where id=18;
+update industry_type set name='教育培训' where id=19;
+update industry_type set name='其他' where id=20;
+
+
+update industry_type set name='美妆' where id=52;
+update industry_type set name='服装' where id=53;
+update industry_type set name='鞋靴箱包' where id=55;
+update industry_type set name='数码家电' where id=56;
+update industry_type set name='内衣饰品' where id=57;
+update industry_type set name='汽车用品' where id=58;
+update industry_type set name='其他' where id=59;
+delete from industry_type where id >=60 and id <=65;
