da39075f54d3218323117e70478c0a3e043d7657
sunlinqing <linqing_sun@lovelawu.com>
2017-10-24 17:22:28 +0800

    feat: 添加统一派号器模块id-worker、id-worker-client

---
+++ common-impl/id-worker-client/src/main/java/com/lawu/eshop/idworker/client/BizIdType.java
@@ -0,0 +1,25 @@
+package com.lawu.eshop.idworker.client;
+
+/**
+ * 业务ID类型
+ * @author Leach
+ * @date 2017/10/24
+ */
+public enum BizIdType {
+    // 会员
+    MEMBER("M"),
+    // 商家
+    MERCHANT("B"),
+    // 订单
+    ORDER("O");
+
+    private String prefix;
+
+    BizIdType(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+}
---
+++ common-impl/id-worker-client/src/main/java/com/lawu/eshop/idworker/client/IdWorkerHelper.java
@@ -0,0 +1,12 @@
+package com.lawu.eshop.idworker.client;
+
+/**
+ * @author Leach
+ * @date 2017/10/24
+ */
+public class IdWorkerHelper {
+
+    public static String generate(BizIdType bizIdType) {
+        return null;
+    }
+}
