fa28b545bfb5f9f090ecc4bccb537fb48a26b132
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-11-14 10:55:33 +0800

    refactor(property-srv):余额统计定时任务优化

--- service-share/property-srv/src/main/java/com/lawu/eshop/property/srv/mapper/extend/RechargeDOMapperExtend.java
+++ service-share/property-srv/src/main/java/com/lawu/eshop/property/srv/mapper/extend/RechargeDOMapperExtend.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.lawu.eshop.property.srv.domain.RechargeDOExample;
 import com.lawu.eshop.property.srv.domain.extend.AreaRechargePointDOView;
+import com.lawu.eshop.property.srv.domain.extend.RechargeDOView;
+import com.lawu.eshop.property.srv.domain.extend.RechargeReportDOView;
 import com.lawu.eshop.property.srv.domain.extend.ReportAreaRechargeDOExtend;
 import com.lawu.eshop.property.srv.domain.extend.ReportAreaRechargeDOView;
 
@@ -12,4 +15,13 @@ public interface RechargeDOMapperExtend {
 
 	List<ReportAreaRechargeDOExtend> selectAgentAreaReportRechargeListByDate(ReportAreaRechargeDOView recharge);
 	List<AreaRechargePointDOView> selectAreaRechargePoint(@Param("bdate")String bdate, @Param("edate")String edate);
+	
+	/**
+	 * 充值余额查询统计
+	 * @param example
+	 * @return
+	 * @author zhangrc
+	 * @date 2017/11/13
+	 */
+	List<RechargeDOView> selectWithdrawCashListByDateAndStatus(RechargeReportDOView example);
 }
