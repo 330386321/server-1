dbeb03491f82c5342b585504589d0cf23d8bbb4e
zhangrongcheng <rongcheng.zhang@lovelawu.com>
2017-11-28 15:31:07 +0800

    test(ad-srv):E咻命中率单元测试

--- service-share/ad-srv/src/test/resources/sql/ddl.sql
+++ service-share/ad-srv/src/test/resources/sql/ddl.sql
@@ -267,3 +267,11 @@ CREATE TABLE `take_platform_red_packet` (
   `gmt_create` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ;
+
+CREATE TABLE `ad_rate_setting` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键' ,
+  `game_time` int(5) unsigned NOT NULL COMMENT '游戏时长（单位秒）',
+  `rate` int(5) NOT NULL COMMENT '对应命中率0~100',
+  `gmt_create` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ;
