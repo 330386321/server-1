003b7ee1eb4693036bc259351649ff119456da46
jiangxinjun <xinjun.jiang@lovelawu.com>
2017-12-11 16:13:29 +0800

    fix(sql):修改执行异常的sql语句

--- sql/1.3.16.4/product_20171208抢购活动表添加索引.sql
+++ sql/1.3.16.4/product_20171208抢购活动表添加索引.sql
@@ -1,4 +1,4 @@
 -- 添加索引，优化查询
 use eshop_product;
 ALTER TABLE `seckill_activity`
-KEY `idx_start_date_activity_status_status` (`start_date`,`activity_status`,`status`) USING BTREE;
+ADD KEY `idx_start_date_activity_status_status` (`start_date`,`activity_status`,`status`) USING BTREE;
